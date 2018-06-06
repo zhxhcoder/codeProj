c54efd7f9aa857a907025bc7b62c2214447c5db1
zhxh <zhuxiaohui@niuguwang.com>
2018-06-06 19:14:25 +0800

    AlterThread

---
+++ app/src/main/java/com/cy/android/codeproj/multithread/AlterThread.java
@@ -0,0 +1,37 @@
+package com.cy.android.codeproj.multithread;
+
+public class AlterThread {
+    public static void main(String[] args) {
+        AlterThread test = new AlterThread();
+
+        Object lock = new Object();
+
+        test.new Output(lock, "A").start();
+        test.new Output(lock, "B").start();
+    }
+
+    class Output extends Thread {
+        private Object lock;
+        private String value;
+
+        public Output(Object lock, String value) {
+            this.lock = lock;
+            this.value = value;
+        }
+
+        @Override
+        public void run() {
+            synchronized (lock) {
+                while (true) {
+                    try {
+                        System.out.println(value);
+                        lock.notifyAll();
+                        lock.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+}
