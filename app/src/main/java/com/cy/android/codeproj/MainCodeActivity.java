2e6cacad428905efa340f4ef8dc4bdbaee327733
zhxh <zhuxiaohui@niuguwang.com>
2018-06-06 10:37:49 +0800

    Initial commit

---
+++ app/src/main/java/com/cy/android/codeproj/MainCodeActivity.java
@@ -0,0 +1,13 @@
+package com.cy.android.codeproj;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainCodeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_code);
+    }
+}
---
+++ app/src/main/java/com/cy/android/codeproj/TestMainCode.java
@@ -0,0 +1,7 @@
+package com.cy.android.codeproj;
+
+public class TestMainCode {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
