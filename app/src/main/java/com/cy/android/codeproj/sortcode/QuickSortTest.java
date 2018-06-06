a39af62c262f8162b92166b21113cf129e41ea32
zhxh <zhuxiaohui@niuguwang.com>
2018-06-06 11:40:14 +0800

    1，QuickSortTest

--- app/src/main/java/com/cy/android/codeproj/sortcode/QuickSortTest.java
+++ app/src/main/java/com/cy/android/codeproj/sortcode/QuickSortTest.java
@@ -4,21 +4,19 @@ public class QuickSortTest {
 
     public static void main(String[] args) {
         int[] arr = {0, 0, 0, 4, 5, 1, 5, 1, 5, 1, 5, 1};
-        quickSort(arr);
+        quickSort(arr, 0, arr.length - 1);
         for (int a : arr) {
             System.out.println(a);
         }
     }
 
-    public static void quickSort(int[] arr) {
-        qsort(arr, 0, arr.length - 1);
-    }
+    private static void quickSort(int[] arr, int low, int high) {
+        if (low == high || low == (high - 1)) return;
 
-    private static void qsort(int[] arr, int low, int high) {
         if (low < high) {
             int pivot = partition(arr, low, high);              //将数组分为两部分
-            qsort(arr, low, pivot - 1);                   //递归排序左子数组
-            qsort(arr, pivot + 1, high);                  //递归排序右子数组
+            quickSort(arr, low, pivot - 1);                   //递归排序左子数组
+            quickSort(arr, pivot + 1, high);                  //递归排序右子数组
         }
     }
 
