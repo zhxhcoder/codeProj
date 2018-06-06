dc60e98ce53eefbe506b6eeebe9004a56438a0e0
zhxh <zhuxiaohui@niuguwang.com>
2018-06-06 19:22:10 +0800

    AlterThreadTest

---
+++ app/src/main/java/com/cy/android/codeproj/multithread/AlterThreadTest.java
@@ -0,0 +1,56 @@
+package com.cy.android.codeproj.multithread;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class AlterThreadTest {
+    private ReentrantLock lock = new ReentrantLock();
+    Condition aCondition = lock.newCondition();
+    Condition bCondition = lock.newCondition();
+
+    public static void main(String[] args) {
+        AlterThreadTest test = new AlterThreadTest();
+
+        test.new AOutput().start();
+        test.new BOutput().start();
+    }
+
+    class AOutput extends Thread {
+
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    lock.lock();
+                    System.out.println("A");
+
+                    bCondition.signal();
+                    aCondition.await();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    lock.unlock();
+                }
+            }
+        }
+    }
+
+    class BOutput extends Thread {
+
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    lock.lock();
+                    System.out.println("B");
+                    aCondition.signal();
+                    bCondition.await();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    lock.unlock();
+                }
+            }
+        }
+    }
+}
