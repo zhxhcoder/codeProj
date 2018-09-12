4645ee5347a6cf251350781279c75d018def6a4e
zhxh <zhxhcoder@gmail.com>
2018-09-12 10:44:56 +0800

    设计模式-builder模式与proxy模式

--- app/src/main/java/com/zhxh/codeproj/rxcourse/RxTest.java
+++ app/src/main/java/com/zhxh/codeproj/rxcourse/RxTest.java
@@ -13,54 +13,6 @@ import io.reactivex.disposables.Disposable;
  */
 public class RxTest {
 
-    static String TAG = "";
-    static String result = "";
 
-    public static void main(String[] args) {
-        if (true) {
-            return;
-        }
-
-        Observer<String> observer = new Observer<String>() {
-            @Override
-            public void onSubscribe(Disposable d) {
-                Log.i(TAG, "onSubscribe: " + d);
-                result += "onSubscribe: " + d + "\n";
-            }
-
-            @Override
-            public void onNext(String string) {
-                Log.i(TAG, "onNext: " + string);
-                result += "onNext: " + string + "\n";
-            }
-
-            @Override
-            public void onError(Throwable e) {
-                Log.i(TAG, "onError: " + e);
-                result += "onError: " + e + "\n";
-            }
-
-            @Override
-            public void onComplete() {
-                Log.i(TAG, "onComplete: ");
-                result += "onComplete: " + "\n";
-            }
-        };
-        Observable<String> observable = Observable.create(new ObservableOnSubscribe<String>() {
-            @Override
-            public void subscribe(ObservableEmitter<String> emitter) throws Exception {
-                emitter.onNext("Hello");
-                emitter.onNext("Rxjava2");
-                emitter.onNext("My name is zhxh");
-                emitter.onNext("What's your name");
-                //一旦调用onComplete,下面将不在接受事件
-                emitter.onComplete();
-            }
-        });
-
-        observable.subscribe(observer);
-
-        System.out.println("**********************************************************************************************");
-    }
 
 }
