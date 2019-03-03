60a1fda17aa94c85fa4e05c687c0615a11d09843
zhxhcoder <zhxhcoder@gmail.com>
2019-03-03 14:44:08 +0800

    链表反转

---
+++ app/src/main/java/com/zhxh/codeproj/algorithm/ReverseLinkedList.java
@@ -0,0 +1,15 @@
+package com.zhxh.codeproj.algorithm;
+
+public class ReverseLinkedList {
+
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+
+}
