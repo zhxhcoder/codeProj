a895646ed2b371e99055458c8cd8424edb482a8c
zhxhcoder <zhxhcoder@gmail.com>
2019-03-03 14:50:32 +0800

    链表反转

--- app/src/main/java/com/zhxh/codeproj/algorithm/ReverseLinkedList.java
+++ app/src/main/java/com/zhxh/codeproj/algorithm/ReverseLinkedList.java
@@ -2,23 +2,34 @@ package com.zhxh.codeproj.algorithm;
 
 public class ReverseLinkedList {
 
-    class ListNode {
+    class Node {
         int val;
-        ListNode next;
-        ListNode(int x) {
+        Node next;
+
+        Node(int x) {
             val = x;
             next = null;
         }
     }
 
     // 1.就地反转法
-    public ListNode reverseList1(ListNode head) {
+    /*
+     把当前链表的下一个节点pCur插入到头结点dummy的下一个节点中，就地反转。
+
+     dummy->1->2->3->4->5的就地反转过程：
+
+     dummy->2->1->3->4->5
+     dummy->3->2->1->4->5
+     dummy->4>-3->2->1->5
+     dummy->5->4->3->2->1
+     */
+    public Node reverseList1(Node head) {
         if (head == null)
             return head;
-        ListNode dummy = new ListNode(-1);
+        Node dummy = new Node(-1);
         dummy.next = head;
-        ListNode prev = dummy.next;
-        ListNode pCur = prev.next;
+        Node prev = dummy.next;
+        Node pCur = prev.next;
         while (pCur != null) {
             prev.next = pCur.next;
             pCur.next = dummy.next;
@@ -29,11 +40,12 @@ public class ReverseLinkedList {
     }
 
     // 2.新建链表,头节点插入法
-    public ListNode reverseList2(ListNode head) {
-        ListNode dummy = new ListNode(-1);
-        ListNode pCur = head;
+    //新建一个头结点，遍历原链表，把每个节点用头结点插入到新建链表中。最后，新建的链表就是反转后的链表。
+    public Node reverseList2(Node head) {
+        Node dummy = new Node(-1);
+        Node pCur = head;
         while (pCur != null) {
-            ListNode pNex = pCur.next;
+            Node pNex = pCur.next;
             pCur.next = dummy.next;
             dummy.next = pCur;
             pCur = pNex;
