44221a479ccfa7bfb7c2db18ced194bbf12a2e13
zhxhcoder <zhxhcoder@gmail.com>
2019-03-03 14:45:47 +0800

    链表反转

--- app/src/main/java/com/zhxh/codeproj/algorithm/ReverseLinkedList.java
+++ app/src/main/java/com/zhxh/codeproj/algorithm/ReverseLinkedList.java
@@ -11,5 +11,33 @@ public class ReverseLinkedList {
         }
     }
 
+    // 1.就地反转法
+    public ListNode reverseList1(ListNode head) {
+        if (head == null)
+            return head;
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode prev = dummy.next;
+        ListNode pCur = prev.next;
+        while (pCur != null) {
+            prev.next = pCur.next;
+            pCur.next = dummy.next;
+            dummy.next = pCur;
+            pCur = prev.next;
+        }
+        return dummy.next;
+    }
 
+    // 2.新建链表,头节点插入法
+    public ListNode reverseList2(ListNode head) {
+        ListNode dummy = new ListNode(-1);
+        ListNode pCur = head;
+        while (pCur != null) {
+            ListNode pNex = pCur.next;
+            pCur.next = dummy.next;
+            dummy.next = pCur;
+            pCur = pNex;
+        }
+        return dummy.next;
+    }
 }
