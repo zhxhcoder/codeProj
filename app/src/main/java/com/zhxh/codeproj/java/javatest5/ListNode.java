50b56b70188e793c41b7342397d582f72a0a6450
zhxhcoder <zhxhcoder@gmail.com>
2019-04-04 17:34:53 +0800

    链表合并

---
+++ app/src/main/java/com/zhxh/codeproj/java/javatest5/ListNode.java
@@ -0,0 +1,12 @@
+package com.zhxh.codeproj.java.javatest5;
+
+/**
+ * Created by zhxh on 2019/4/4
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+    }
+}
