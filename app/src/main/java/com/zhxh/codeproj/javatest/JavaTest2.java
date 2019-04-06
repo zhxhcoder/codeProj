2a6b77cabd723ca444aa63f2d0e42732292afb55
zhxhcoder <zhxhcoder@gmail.com>
2019-04-06 12:23:31 +0800

    执行顺序

--- app/src/main/java/com/zhxh/codeproj/javatest/JavaTest2.java
+++ app/src/main/java/com/zhxh/codeproj/javatest/JavaTest2.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 /**
  * Created by zhxh on 2019/4/3
  * [6, 8, 10, 12]
+ * [6, 8, 10, 12]
  */
 public class JavaTest2 {
     private static void changeValue(int[] arr) {
@@ -13,9 +14,18 @@ public class JavaTest2 {
         }
     }
 
+    private static void changeValue(Integer[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] *= 2;
+        }
+    }
+
     public static void main(String[] args) {
-        int[] arr = {3, 4, 5, 6};
-        changeValue(arr);
-        System.out.println(Arrays.toString(arr));
+        int[] arr1 = {3, 4, 5, 6};
+        Integer[] arr2 = {3, 4, 5, 6};
+        changeValue(arr1);
+        changeValue(arr2);
+        System.out.println(Arrays.toString(arr1));
+        System.out.println(Arrays.toString(arr2));
     }
 }
