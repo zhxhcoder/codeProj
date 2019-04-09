7c4f6b2f122ce86f969cb23c267df30f2c7c4579
zhxhcoder <zhxhcoder@gmail.com>
2019-04-09 16:57:40 +0800

    proxy

---
+++ app/src/main/java/com/zhxh/codeproj/designpattern/proxy/RealSubject.java
@@ -0,0 +1,11 @@
+package com.zhxh.codeproj.designpattern.proxy;
+
+/**
+ * Created by zhxh on 2019/4/9
+ */
+public class RealSubject implements Subject {
+    @Override
+    public void request() {
+        //todo something
+    }
+}
