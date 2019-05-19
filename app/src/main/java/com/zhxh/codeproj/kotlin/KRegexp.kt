04a31b7209d89523683fa81d138e9da25a35b364
zhxh <zhxhcoder@gmail.com>
2019-05-19 18:00:53 +0800

    命令模式

--- app/src/main/java/com/zhxh/codeproj/kotlin/KRegexp.kt
+++ app/src/main/java/com/zhxh/codeproj/kotlin/KRegexp.kt
@@ -10,6 +10,8 @@ object KRegexp {
         val s1 = Regex("\\d+").findAll("6732eed3").toList()
         val s2 = Regex("""\D+""").findAll("6732eed3").toList()
         val s3 = Regex("""\d+(?=$)""").findAll("6732eed3").toList()
+        val s4 = Regex("""\d+$""").findAll("6732eed3").toList()
+        val s5 = Regex("""\d+$""").find("6732eed3")
         
         s1.forEach {
             println("匹配值 " + it.value)
@@ -26,5 +28,12 @@ object KRegexp {
             println(it.range.start)
             println(it.range.endInclusive)
         }
+        s4.forEach {
+            println("匹配值 " + it.value)
+            println(it.range.start)
+            println(it.range.endInclusive)
+        }
+        println("********")
+        println(s5?.groupValues)
     }
 }
