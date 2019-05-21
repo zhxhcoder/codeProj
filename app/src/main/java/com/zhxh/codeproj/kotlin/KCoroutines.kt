afdfdbe809230b5c67e5bc5383d34a726393449d
zhxh <zhxhcoder@gmail.com>
2019-05-21 11:57:13 +0800

    kotlin协程总结

--- app/src/main/java/com/zhxh/codeproj/kotlin/KCoroutines.kt
+++ app/src/main/java/com/zhxh/codeproj/kotlin/KCoroutines.kt
@@ -8,14 +8,43 @@ import kotlinx.coroutines.*
 object KCoroutines {
     @JvmStatic
     fun main(args: Array<String>) {
-        
+
         GlobalScope.launch {
             //新建一个后台协程
             delay(5000L) //非阻塞延迟5秒
             println("协程打印") // print after delay
         }
-        
-        println("主线程打印")
-        Thread.sleep(5100L) // 主线程阻塞以JVM保持激活 如果时间小于协程延迟则不输入协程打印
+
+        val job = GlobalScope.launch(Dispatchers.Default) {
+            //在一个协程环境中，执行后台耗时操作
+            //新建一个后台协程
+            delay(3000L) //非阻塞延迟5秒
+            println("Default协程打印") // print after delay
+        }
+
+        GlobalScope.launch(newSingleThreadContext("MyThread")) {
+            //在一个协程环境中，执行后天耗时操作
+            //newSingleThreadContext 会为协程运行启动一个新定线程。既然起了新定线程，那么协程就没啥意义了
+            delay(2000L) //非阻塞延迟5秒
+            println("newSingleThreadContext协程打印") // print after delay
+        }
+
+        //正常在Java中，我们想在thread中得到返回值。一般会通过callback
+
+        GlobalScope.launch {
+            val result = async {
+                function()
+            }
+            println("zzzz-${result.await()}")
+        }
+
+        job.cancel()
+
+        Thread.sleep(9100L) // 主线程阻塞以JVM保持激活 如果时间小于协程延迟则不输入协程打印
     }
+
+    private fun function(): Int {
+        return 999
+    }
+
 }
