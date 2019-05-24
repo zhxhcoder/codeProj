b73e448b7e6ec9d01736fe2bdbff1916871d7edb
zhxh <zhxhcoder@gmail.com>
2019-05-24 15:25:10 +0800

    let ?:判空

--- app/src/main/java/com/zhxh/codeproj/kotlin/TestKotlin.kt
+++ app/src/main/java/com/zhxh/codeproj/kotlin/TestKotlin.kt
@@ -7,20 +7,40 @@ package com.zhxh.codeproj.kotlin
 object TestKotlin {
 
 
+    fun print1() {
+        val maxElement = emptyElements()?.let { it.max() } ?: return
+        println("#1#最大值 $maxElement")
+    }
+
+    fun print2() {
+        val list: List<Int>? = emptyElements()
+        val maxElement = if (list != null) list.max() else return
+        println("#2#最大值 $maxElement")
+    }
+
+    fun emptyElements(): List<Int>? = emptyList()
+
     @JvmStatic
     fun main(args: Array<String>) {
-        val list1: List<Int>? = emptyElements()
+        val list1: List<Int>? = createElements()
         val maxElement1 = list1?.let { it.max() } ?: return
         println("#1#最大值 $maxElement1")
 
         println("************************************")
 
-        val list2: List<Int>? = emptyElements()
+        val list2: List<Int>? = createElements()
         val maxElement2 = if (list2 != null) list2.max() else return
         println("#2#最大值 $maxElement2")
+
+        println("*****************let ?:判空*******************")
+
+        print1()
+
+        println("***************if else 判空*********************")
+
+        print2()
     }
 
-    fun emptyElements(): List<Int>? = emptyList()
     private fun createElements(): List<Int> {
         return listOf(11, 33, 22)
     }
