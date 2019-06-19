03befc257297eed6a4bca37c3ead303ab35734da
zhxh <zhxhcoder@gmail.com>
2019-06-19 11:44:34 +0800

    MyNetService测试

--- app/src/main/java/com/zhxh/codeproj/javatest/java7/MyNetService.java
+++ app/src/main/java/com/zhxh/codeproj/javatest/java7/MyNetService.java
@@ -5,6 +5,10 @@ package com.zhxh.codeproj.javatest.java7;
  * Created by zhxh on 2019/06/19
  */
 public class MyNetService extends BaseNetService {
+    @Override
+    public String getCallAdapterFactory() {
+        return "kotlin";
+    }
 
     @Override
     public String getBaseUrl() {
