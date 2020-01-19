f59369f54e80e6b84f81de72b8dd10609e02c2ca
zhxh <zhxhcoder@gmail.com>
2020-01-19 17:50:41 +0800

    三个值之和为0

--- app/src/main/java/com/zhxh/codeproj/leetcode/LeetCode15.java
+++ app/src/main/java/com/zhxh/codeproj/leetcode/LeetCode15.java
@@ -4,6 +4,18 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/*
+ * 算法流程：
+ * 特判，对于数组长度 nn，如果数组为 nullnull 或者数组长度小于 33，返回 [][]。
+ * 对数组进行排序。
+ * 遍历排序后数组：
+ * 若 nums[i]>0nums[i]>0：因为已经排序好，所以后面不可能有三个数加和等于 00，直接返回结果。
+ * 对于重复元素：跳过，避免出现重复解
+ * 令左指针 L=i+1L=i+1，右指针 R=n-1R=n−1，当 L<RL<R 时，执行循环：
+ * 当 nums[i]+nums[L]+nums[R]==0nums[i]+nums[L]+nums[R]==0，执行循环，判断左界和右界是否和下一位置重复，去除重复解。并同时将 L,RL,R 移到下一位置，寻找新的解
+ * 若和大于 00，说明 nums[R]nums[R] 太大，RR 左移
+ * 若和小于 00，说明 nums[L]nums[L] 太小，LL 右移
+ */
 public class LeetCode15 {
     public static void main(String[] args) {
         int[] height = {-1, 0, 1, 2, -1, -4};
