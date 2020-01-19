88f4f5f928714ee546e7ac8bf7f712c25f5abdf1
zhxh <zhxhcoder@gmail.com>
2020-01-19 11:09:08 +0800

    计算最大值

--- app/src/main/java/com/zhxh/codeproj/leetcode/LeetCode11.java
+++ app/src/main/java/com/zhxh/codeproj/leetcode/LeetCode11.java
@@ -2,13 +2,9 @@ package com.zhxh.codeproj.leetcode;
 
 public class LeetCode11 {
     public static void main(String[] args) {
-
         int[] height = {1, 8, 6, 2, 5, 4, 8, 3, 7};
-
         Solution solution = new Solution();
-
         int maxArea = solution.maxArea(height);
-
         System.out.println("maxArea:" + maxArea);
     }
 
