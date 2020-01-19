28cf39693827d1091c6f53b9abb19e0d0f88284b
zhxh <zhxhcoder@gmail.com>
2020-01-19 16:02:16 +0800

    计算面积最大值

--- app/src/main/java/com/zhxh/codeproj/leetcode/LeetCode11.java
+++ app/src/main/java/com/zhxh/codeproj/leetcode/LeetCode11.java
@@ -2,7 +2,7 @@ package com.zhxh.codeproj.leetcode;
 
 public class LeetCode11 {
     public static void main(String[] args) {
-        int[] height = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+        int[] height = {8, 10, 2, 2, 2, 2, 2, 99, 9};
         Solution solution = new Solution();
         int maxArea = solution.maxArea(height);
         System.out.println("maxArea:" + maxArea);
@@ -13,6 +13,9 @@ public class LeetCode11 {
             int maxarea = 0, l = 0, r = height.length - 1;
             while (l < r) {
                 maxarea = Math.max(maxarea, Math.min(height[l], height[r]) * (r - l));
+                //最初我们考虑由最外面两条线段构成的区域。现在，为是使面积最大化，我们需要考虑更长的两条线段之间的区域。
+                //如果我们试图将指向较长线段的指针向内侧移动，矩形区域的面积将受限于较短的线段而不会获得任何增加。但是，在同样的条件下，移动
+                //较短的指针尽管造成了矩形宽度的减小，但却可能有助于面积的增大。因为移动较短的指针会得到相对较长的线段，这可以克服由宽度减小而引起的面积减少。
                 if (height[l] < height[r])
                     l++;
                 else
