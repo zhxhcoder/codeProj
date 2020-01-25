777bfa9d335d7c1be535a681f0e29ef3e1097da2
zhxh <zhxhcoder@gmail.com>
2020-01-25 10:03:43 +0800

    爬楼梯

---
+++ app/src/main/java/com/zhxh/codeproj/leetcode/LeetCode70.java
@@ -0,0 +1,26 @@
+package com.zhxh.codeproj.leetcode;
+
+
+public class LeetCode70 {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        System.out.println(solution.climbStairs(9));
+    }
+
+    static class Solution {
+        public int climbStairs(int n) {
+            if (n == 1) {
+                return 1;
+            }
+            int[] dp = new int[n + 1];
+            dp[1] = 1;
+            dp[2] = 2;
+            for (int i = 3; i <= n; i++) {
+                dp[i] = dp[i - 1] + dp[i - 2];
+            }
+            return dp[n];
+        }
+    }
+}
