6c702715a98837a1cea5e1702c81d720637ff02e
zhxh <zhxhcoder@gmail.com>
2020-01-28 10:52:10 +0800

    反转字符串

---
+++ app/src/main/java/com/zhxh/codeproj/leetcode/LeetCode344.java
@@ -0,0 +1,15 @@
+package com.zhxh.codeproj.leetcode;
+
+public class LeetCode344 {
+
+    class Solution {
+        public void reverseString(char[] s) {
+            int left = 0, right = s.length - 1;
+            while (left < right) {
+                char tmp = s[left];
+                s[left++] = s[right];
+                s[right--] = tmp;
+            }
+        }
+    }
+}
