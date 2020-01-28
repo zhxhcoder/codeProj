b9da91bbfbb9534791fbba0f6e257d0397368575
zhxh <zhxhcoder@gmail.com>
2020-01-28 10:40:51 +0800

    对称二叉树

--- app/src/main/java/com/zhxh/codeproj/leetcode/LeetCode101.java
+++ app/src/main/java/com/zhxh/codeproj/leetcode/LeetCode101.java
@@ -10,7 +10,26 @@ public class LeetCode101 {
      * TreeNode(int x) { val = x; }
      * }
      */
-    class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        Solution.TreeNode node1 = new Solution.TreeNode(1);
+        Solution.TreeNode node2 = new Solution.TreeNode(2);
+        Solution.TreeNode node3 = new Solution.TreeNode(3);
+        Solution.TreeNode node4 = new Solution.TreeNode(4);
+        Solution.TreeNode node5 = new Solution.TreeNode(5);
+
+        node1.left = node2;
+        node1.right = node3;
+        node2.left = node4;
+        node2.right = node5;
+
+
+        System.out.println(solution.isSymmetric(node1));
+
+    }
+
+    static class Solution {
         public boolean isSymmetric(TreeNode root) {
             return isMirror(root, root);
         }
@@ -23,7 +42,7 @@ public class LeetCode101 {
                     && isMirror(t1.left, t2.right);
         }
 
-        public class TreeNode {
+        static public class TreeNode {
             int val;
             TreeNode left;
             TreeNode right;
