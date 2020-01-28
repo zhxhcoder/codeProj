82e78a276bd3673862ad3862400af14720ed4141
zhxh <zhxhcoder@gmail.com>
2020-01-28 10:57:18 +0800

    反转字符串

--- app/src/main/java/com/zhxh/codeproj/leetcode/LeetCode344.java
+++ app/src/main/java/com/zhxh/codeproj/leetcode/LeetCode344.java
@@ -2,7 +2,24 @@ package com.zhxh.codeproj.leetcode;
 
 public class LeetCode344 {
 
-    class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        char[] s = {'h', 'e', 'l', 'l', 'o'};
+
+        for (char i : s) {
+            System.out.print(i);
+        }
+        solution.reverseString(s);
+
+        System.out.print("\n******\n");
+
+        for (char i : s) {
+            System.out.print(i);
+        }
+    }
+
+    static class Solution {
         public void reverseString(char[] s) {
             int left = 0, right = s.length - 1;
             while (left < right) {
