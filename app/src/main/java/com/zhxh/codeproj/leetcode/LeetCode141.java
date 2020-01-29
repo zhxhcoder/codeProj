8cdf9563b9710b41d043e152b04639d04b91c6e6
zhxh <zhxhcoder@gmail.com>
2020-01-29 19:54:09 +0800

    环形链表

---
+++ app/src/main/java/com/zhxh/codeproj/leetcode/LeetCode141.java
@@ -0,0 +1,31 @@
+package com.zhxh.codeproj.leetcode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LeetCode141 {
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public class Solution {
+        public boolean hasCycle(ListNode head) {
+            Set<ListNode> nodesSeen = new HashSet<>();
+            while (head != null) {
+                if (nodesSeen.contains(head)) {
+                    return true;
+                } else {
+                    nodesSeen.add(head);
+                }
+                head = head.next;
+            }
+            return false;
+        }
+    }
+}
