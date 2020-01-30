14be5fb2cd3bcec191bc8c98eb9bc5cbf9a79b1f
zhxh <zhxhcoder@gmail.com>
2020-01-30 14:07:54 +0800

    移动零

--- app/src/main/java/com/zhxh/codeproj/leetcode/LeetCode283.java
+++ app/src/main/java/com/zhxh/codeproj/leetcode/LeetCode283.java
@@ -4,9 +4,18 @@ public class LeetCode283 {
 
     public static void main(String[] args) {
 
+        Solution solution = new Solution();
+
+        int[] nums = {3, 4, 2, 0, 0, 3, 1, 2, 6, 0};
+
+        solution.moveZeroes(nums);
+
+        for (int item : nums) {
+            System.out.println(item);
+        }
     }
 
-    class Solution {
+    static class Solution {
         public void moveZeroes(int[] nums) {
             int index = 0;
             for (int i = 0; i < nums.length; i++) {
