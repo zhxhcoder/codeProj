18246dcc3b7559d04f4e1b7f86f4c3edae22e7d3
zhxh <zhxhcoder@gmail.com>
2020-01-30 10:42:20 +0800

    二叉树最大深度

---
+++ app/src/main/java/com/zhxh/codeproj/leetcode/LeetCode104.java
@@ -0,0 +1,43 @@
+package com.zhxh.codeproj.leetcode;
+
+public class LeetCode104 {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        TreeNode node1 = new TreeNode(1);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node4 = new TreeNode(4);
+        TreeNode node5 = new TreeNode(5);
+
+        node1.left = node2;
+        node1.right = node3;
+        node2.left = node4;
+        node2.right = node5;
+
+        System.out.println(solution.maxDepth(node1));
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    static class Solution {
+        public int maxDepth(TreeNode root) {
+            if (root == null) {
+                return 0;
+            } else {
+                int left_height = maxDepth(root.left);
+                int right_height = maxDepth(root.right);
+                return java.lang.Math.max(left_height, right_height) + 1;
+            }
+        }
+    }
+}
