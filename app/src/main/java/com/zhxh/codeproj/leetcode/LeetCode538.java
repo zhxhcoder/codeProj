4db118f864ae8212614d2bc2ae8ece3aa1d86830
zhxh <zhxhcoder@gmail.com>
2020-01-31 13:53:10 +0800

    把二叉搜索树转换为累加树

---
+++ app/src/main/java/com/zhxh/codeproj/leetcode/LeetCode538.java
@@ -0,0 +1,27 @@
+package com.zhxh.codeproj.leetcode;
+
+public class LeetCode538 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    class Solution {
+        private int sum = 0;
+
+        public TreeNode convertBST(TreeNode root) {
+            if (root != null) {
+                convertBST(root.right);
+                sum += root.val;
+                root.val = sum;
+                convertBST(root.left);
+            }
+            return root;
+        }
+    }
+}
