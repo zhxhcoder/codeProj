1f00718f63b2440fa109b8c16b390316a1e70e18
zhxh <zhxhcoder@gmail.com>
2020-01-31 14:22:44 +0800

    打劫

--- app/src/main/java/com/zhxh/codeproj/leetcode/LeetCode198.java
+++ app/src/main/java/com/zhxh/codeproj/leetcode/LeetCode198.java
@@ -1,14 +1,25 @@
 package com.zhxh.codeproj.leetcode;
 
 public class LeetCode198 {
-    public int rob(int[] num) {
-        int prevMax = 0;
-        int currMax = 0;
-        for (int x : num) {
-            int temp = currMax;
-            currMax = Math.max(prevMax + x, currMax);
-            prevMax = temp;
+
+    public static void main(String[] args) {
+        int[] nums = {3, 3, 24, 5, 2, 1, 9};
+
+        Solution solution = new Solution();
+
+        System.out.println(solution.rob(nums));
+    }
+
+    static class Solution {
+        public int rob(int[] nums) {
+            int prevMax = 0;
+            int currMax = 0;
+            for (int x : nums) {
+                int temp = currMax;
+                currMax = Math.max(prevMax + x, currMax);
+                prevMax = temp;
+            }
+            return currMax;
         }
-        return currMax;
     }
 }
