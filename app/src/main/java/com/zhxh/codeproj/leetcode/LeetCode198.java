de15b8fc03e49f62ba77ac22399bdc52bd8f9ba2
zhxh <zhxhcoder@gmail.com>
2020-01-31 14:17:35 +0800

    打劫

---
+++ app/src/main/java/com/zhxh/codeproj/leetcode/LeetCode198.java
@@ -0,0 +1,14 @@
+package com.zhxh.codeproj.leetcode;
+
+public class LeetCode198 {
+    public int rob(int[] num) {
+        int prevMax = 0;
+        int currMax = 0;
+        for (int x : num) {
+            int temp = currMax;
+            currMax = Math.max(prevMax + x, currMax);
+            prevMax = temp;
+        }
+        return currMax;
+    }
+}
