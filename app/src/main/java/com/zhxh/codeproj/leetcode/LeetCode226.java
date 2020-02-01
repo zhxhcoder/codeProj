510d337a962cb1cb296b59b7b2c6269e06d52c7a
zhxh <zhxhcoder@gmail.com>
2020-02-01 10:08:42 +0800

    翻转二叉树

---
+++ app/src/main/java/com/zhxh/codeproj/leetcode/LeetCode226.java
@@ -0,0 +1,27 @@
+package com.zhxh.codeproj.leetcode;
+
+public class LeetCode226 {
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    class Solution {
+        public TreeNode invertTree(TreeNode root) {
+            if (root == null) {
+                return null;
+            }
+            TreeNode right = invertTree(root.right);
+            TreeNode left = invertTree(root.left);
+            root.left = right;
+            root.right = left;
+            return root;
+        }
+    }
+}
