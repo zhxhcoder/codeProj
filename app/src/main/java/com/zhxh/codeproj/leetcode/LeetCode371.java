dcc2aab53a248f08c0b15e4e36c06b944b0a28c7
zhxh <zhxhcoder@gmail.com>
2020-02-16 12:26:41 +0800

    Excel表列序号

--- app/src/main/java/com/zhxh/codeproj/leetcode/LeetCode371.java
+++ app/src/main/java/com/zhxh/codeproj/leetcode/LeetCode371.java
@@ -3,9 +3,16 @@ package com.zhxh.codeproj.leetcode;
 public class LeetCode371 {
 
     public static void main(String[] args) {
+
+        int a=32;
+        int b=4;
+
+        Solution solution=new Solution();
+
+        System.out.println(solution.getSum(a,b));
     }
 
-    class Solution {
+   static class Solution {
         public int getSum(int a, int b) {
             while (b != 0) {
                 int temp = a ^ b;
