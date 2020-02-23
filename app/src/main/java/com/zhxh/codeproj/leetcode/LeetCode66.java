f74826fb6461b48fc7ca784bf947e73195935969
zhxh <zhxhcoder@gmail.com>
2020-02-23 20:05:07 +0800

    加1

---
+++ app/src/main/java/com/zhxh/codeproj/leetcode/LeetCode66.java
@@ -0,0 +1,16 @@
+package com.zhxh.codeproj.leetcode;
+
+public class LeetCode66 {
+    class Solution {
+        public int[] plusOne(int[] digits) {
+            for (int i = digits.length - 1; i >= 0; i--) {
+                digits[i]++;
+                digits[i] = digits[i] % 10;
+                if (digits[i] != 0) return digits;
+            }
+            digits = new int[digits.length + 1];
+            digits[0] = 1;
+            return digits;
+        }
+    }
+}
