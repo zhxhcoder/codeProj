e37be5d37c9b2208443ed9afa3770946ffe9de01
zhxh <zhxhcoder@gmail.com>
2020-06-22 14:22:59 +0800

    正确的快排

--- app/src/main/java/com/zhxh/codeproj/sortcode/QuickSortTest.java
+++ app/src/main/java/com/zhxh/codeproj/sortcode/QuickSortTest.java
@@ -1,39 +1,72 @@
 package com.zhxh.codeproj.sortcode;
 
 import java.util.Arrays;
+import java.util.Random;
 
 public class QuickSortTest {
     public static void main(String[] args) {
-        int[] arr = {0, 3, -10, 4, 5, 1, -5, 1, 5, -1, 5, 1};
+        int[] arr = {1, 3, -2, 5, 4, -3, 2, 6, -1, 1};
         System.out.println(Arrays.toString(arr));
+        System.out.println(arr.length);
+        quickSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    /**
+     * 快速排序，使得整数数组 arr 有序
+     */
+    public static void quickSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
         quickSort(arr, 0, arr.length - 1);
-        System.out.println(Arrays.toString(arr));
     }
 
-    private static void quickSort(int[] arr, int begin, int end) {
-        if (begin == end || begin == (end - 1)) return;
-        if (begin < end) {
-            int pivot = partition(arr, begin, end);              //将数组分为两部分
-            quickSort(arr, begin, pivot - 1);                   //递归排序左子数组
-            quickSort(arr, pivot + 1, end);                  //递归排序右子数组
+    /**
+     * 快速排序，使得整数数组 arr 的 [L, R] 部分有序
+     */
+    public static void quickSort(int[] arr, int L, int R) {
+        if (L < R) {
+            // 把数组中随机的一个元素与最后一个元素交换，这样以最后一个元素作为基准值实际上就是以数组中随机的一个元素作为基准值
+            swap(arr, new Random().nextInt(R - L + 1) + L, R);
+            int[] p = partition(arr, L, R);
+            quickSort(arr, L, p[0] - 1);
+            quickSort(arr, p[1] + 1, R);
         }
     }
 
-    private static int partition(int[] arr, int begin, int end) {
-        int pivot = arr[begin];     //枢轴记录
-        while (begin < end) {
-            while (begin < end && arr[end] >= pivot) {
-                --end;
+    /**
+     * 分区的过程，整数数组 arr 的[L, R]部分上，使得：
+     * 大于 arr[R] 的元素位于[L, R]部分的右边，但这部分数据不一定有序
+     * 小于 arr[R] 的元素位于[L, R]部分的左边，但这部分数据不一定有序
+     * 等于 arr[R] 的元素位于[L, R]部分的中间
+     * 返回等于部分的第一个元素的下标和最后一个下标组成的整数数组
+     */
+    public static int[] partition(int[] arr, int L, int R) {
+
+        int basic = arr[R];
+        int less = L - 1;
+        int more = R + 1;
+        while (L < more) {
+            if (arr[L] < basic) {
+                swap(arr, ++less, L++);
+            } else if (arr[L] > basic) {
+                swap(arr, --more, L);
+            } else {
+                L++;
             }
-            arr[begin] = arr[end]; //交换比枢轴小的记录到左端
-            while (begin < end && arr[begin] <= pivot) {
-                ++begin;
-            }
-            arr[end] = arr[begin];//交换比枢轴小的记录到右端
         }
-        //扫描完成，枢轴到位
-        arr[begin] = pivot;
-        //返回的是枢轴的位置
-        return begin;
+
+        return new int[]{less + 1, more - 1};
+
+    }
+
+    /*
+     * 交换数组 arr 中下标为 i 和下标为 j 位置的元素
+     */
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
