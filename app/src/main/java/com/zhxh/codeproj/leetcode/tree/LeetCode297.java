94c8c37b95529e0f344765c3613ab5acbeb6a521
zhxh <zhxhcoder@gmail.com>
2020-06-23 16:12:08 +0800

    序列化与反序列化

--- app/src/main/java/com/zhxh/codeproj/leetcode/tree/LeetCode297.java
+++ app/src/main/java/com/zhxh/codeproj/leetcode/tree/LeetCode297.java
@@ -1,5 +1,85 @@
 package com.zhxh.codeproj.leetcode.tree;
 
+import com.zhxh.codeproj.leetcode._bean.TreeNode;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+/*
+序列化是将一个数据结构或者对象转换为连续的比特位的操作，进而可以将转换后的数据存储在一个文件或者内存中，同时也可以通过网络传输到另一个计算机环境，采取相反方式重构得到原数据。
+
+请设计一个算法来实现二叉树的序列化与反序列化。这里不限定你的序列 / 反序列化算法执行逻辑，你只需要保证一个二叉树可以被序列化为一个字符串并且将这个字符串反序列化为原始的树结构。
+
+示例: 
+
+你可以将以下二叉树：
+
+    1
+   / \
+  2   3
+     / \
+    4   5
+
+序列化为 "[1,2,3,null,null,4,5]"
+提示: 这与 LeetCode 目前使用的方式一致，详情请参阅 LeetCode 序列化二叉树的格式。你并非必须采取这种方式，你也可以采用其他的方法解决这个问题。
+
+说明: 不要使用类的成员 / 全局 / 静态变量来存储状态，你的序列化和反序列化算法应该是无状态的。
+
+ */
 class LeetCode297 {
+    public static void main(String[] args) {
+
+    }
+    /*
+    方法一：深度优先搜索
+思路和算法
+
+二叉树的序列化本质上是对其值进行编码，更重要的是对其结构进行编码。可以遍历树来完成上述任务。众所周知，我们一般有两个策略：BFS / DFS。
+
+BFS 可以按照层次的顺序从上到下遍历所有的节点
+DFS 可以从一个根开始，一直延伸到某个叶，然后回到根，到达另一个分支。根据根节点、左节点和右节点之间的相对顺序，可以进一步将DFS策略区分为：
+先序遍历
+中序遍历
+后序遍历
+
+     */
+
+    static class Codec {
+        public String rserialize(TreeNode root, String str) {
+            if (root == null) {
+                str += "None,";
+            } else {
+                str += str.valueOf(root.val) + ",";
+                str = rserialize(root.left, str);
+                str = rserialize(root.right, str);
+            }
+            return str;
+        }
+
+        public String serialize(TreeNode root) {
+            return rserialize(root, "");
+        }
+
+        public TreeNode rdeserialize(List<String> l) {
+            if (l.get(0).equals("None")) {
+                l.remove(0);
+                return null;
+            }
+
+            TreeNode root = new TreeNode(Integer.valueOf(l.get(0)));
+            l.remove(0);
+            root.left = rdeserialize(l);
+            root.right = rdeserialize(l);
+
+            return root;
+        }
+
+        public TreeNode deserialize(String data) {
+            String[] data_array = data.split(",");
+            List<String> data_list = new LinkedList<String>(Arrays.asList(data_array));
+            return rdeserialize(data_list);
+        }
+    }
+
 }
