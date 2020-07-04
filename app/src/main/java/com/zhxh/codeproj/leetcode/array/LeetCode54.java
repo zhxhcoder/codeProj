8ed8aea8c601c77db4263583857e59bcb11bae73
zhxh <zhxhcoder@gmail.com>
2020-07-04 16:29:42 +0800

    54. 旋转矩阵

---
+++ app/src/main/java/com/zhxh/codeproj/leetcode/array/LeetCode54.java
@@ -0,0 +1,65 @@
+package com.zhxh.codeproj.leetcode.array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+给定一个包含 m x n 个元素的矩阵（m 行, n 列），请按照顺时针螺旋顺序，返回矩阵中的所有元素。
+
+示例 1:
+
+输入:
+[
+ [ 1, 2, 3 ],
+ [ 4, 5, 6 ],
+ [ 7, 8, 9 ]
+]
+输出: [1,2,3,6,9,8,7,4,5]
+示例 2:
+
+输入:
+[
+  [1, 2, 3, 4],
+  [5, 6, 7, 8],
+  [9,10,11,12]
+]
+输出: [1,2,3,4,8,12,11,10,9,5,6,7]
+
+ */
+class LeetCode54 {
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2, 3, 4};
+        int[] nums2 = {5, 6, 7, 8};
+        int[] nums3 = {9, 10, 11, 12};
+
+        int[][] matrix = {nums1, nums2, nums3};
+
+        System.out.println(new Solution().spiralOrder(matrix));
+    }
+
+    static class Solution {
+        public List<Integer> spiralOrder(int[][] matrix) {
+            List<Integer> order = new ArrayList<>();
+            if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+                return order;
+            }
+            int rows = matrix.length, columns = matrix[0].length;
+            boolean[][] visited = new boolean[rows][columns];
+            int total = rows * columns;
+            int row = 0, column = 0;
+            int[][] directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+            int directionIndex = 0;
+            for (int i = 0; i < total; i++) {
+                order.add(matrix[row][column]);
+                visited[row][column] = true;
+                int nextRow = row + directions[directionIndex][0], nextColumn = column + directions[directionIndex][1];
+                if (nextRow < 0 || nextRow >= rows || nextColumn < 0 || nextColumn >= columns || visited[nextRow][nextColumn]) {
+                    directionIndex = (directionIndex + 1) % 4;
+                }
+                row += directions[directionIndex][0];
+                column += directions[directionIndex][1];
+            }
+            return order;
+        }
+    }
+}
