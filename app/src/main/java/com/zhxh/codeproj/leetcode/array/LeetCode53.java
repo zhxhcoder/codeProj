13b14af281720818c8f62c9dfc5074d2a0d37450
zhxh <zhxhcoder@gmail.com>
2020-07-04 16:25:21 +0800

    整数最大和

--- app/src/main/java/com/zhxh/codeproj/leetcode/array/LeetCode53.java
+++ app/src/main/java/com/zhxh/codeproj/leetcode/array/LeetCode53.java
@@ -18,6 +18,7 @@ public class LeetCode53 {
         Solution solution = new Solution();
         int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
         System.out.println(solution.maxSubArray(nums));
+        System.out.println(new Solution2().maxSubArray(nums));
     }
 
     static class Solution {
@@ -32,5 +33,39 @@ public class LeetCode53 {
             return maxSum;
         }
     }
+
+    static class Solution2 {
+        public class Status {
+            public int lSum, rSum, mSum, iSum;
+
+            public Status(int lSum_, int rSum_, int mSum_, int iSum_) {
+                lSum = lSum_;
+                rSum = rSum_;
+                mSum = mSum_;
+                iSum = iSum_;
+            }
+        }
+
+        public Status pushUp(Status l, Status r) {
+            int iSum = l.iSum + r.iSum;
+            int lSum = Math.max(l.lSum, l.iSum + r.lSum);
+            int rSum = Math.max(r.rSum, r.iSum + l.rSum);
+            int mSum = Math.max(Math.max(l.mSum, r.mSum), l.rSum + r.lSum);
+            return new Status(lSum, rSum, mSum, iSum);
+        }
+
+        public Status getInfo(int[] a, int l, int r) {
+            if (l == r) return new Status(a[l], a[l], a[l], a[l]);
+            int m = (l + r) >> 1;
+            Status lSub = getInfo(a, l, m);
+            Status rSub = getInfo(a, m + 1, r);
+            return pushUp(lSub, rSub);
+        }
+
+        public int maxSubArray(int[] nums) {
+            return getInfo(nums, 0, nums.length - 1).mSum;
+        }
+    }
+
 }
 
