94323e3ab9073c302af0df2bb9aba960e5a4a605
zhxh <zhxhcoder@gmail.com>
2020-07-06 09:06:26 +0800

    相交链表

--- app/src/main/java/com/zhxh/codeproj/leetcode/linkednode/LeetCode160.java
+++ app/src/main/java/com/zhxh/codeproj/leetcode/linkednode/LeetCode160.java
@@ -47,11 +47,15 @@ package com.zhxh.codeproj.leetcode.linkednode;
 程序尽量满足 O(n) 时间复杂度，且仅用 O(1) 内存。
  */
 public class LeetCode160 {
-
-    public class Solution {
+    static class Solution {
         public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+            /**
+             定义两个指针, 第一轮让两个到达末尾的节点指向另一个链表的头部, 最后如果相遇则为交点(在第一轮移动中恰好抹除了长度差)
+             两个指针等于移动了相同的距离, 有交点就返回, 无交点就是各走了两条指针的长度
+             **/
             if (headA == null || headB == null) return null;
             ListNode pA = headA, pB = headB;
+            // 在这里第一轮体现在pA和pB第一次到达尾部会移向另一链表的表头, 而第二轮体现在如果pA或pB相交就返回交点, 不相交最后就是null==null
             while (pA != pB) {
                 pA = pA == null ? headB : pA.next;
                 pB = pB == null ? headA : pB.next;
