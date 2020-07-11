f70efb7120bfeb5c30f4ccb47dba26faece024ea
zhxh <zhxhcoder@gmail.com>
2020-07-11 16:27:37 +0800

    二叉树遍历

--- app/src/main/java/com/zhxh/codeproj/leetcode/tree/Interview32_03.java
+++ app/src/main/java/com/zhxh/codeproj/leetcode/tree/Interview32_03.java
@@ -69,36 +69,42 @@ class Interview32_03 {
             if (root == null) {
                 return new ArrayList<List<Integer>>();
             }
+            //存储最终的结果
             List<List<Integer>> results = new ArrayList<List<Integer>>();
-
-            // add the root element with a delimiter to kick off the BFS loop
+            // 树根添加分隔符以跳出BFS loop循环
             LinkedList<TreeNode> node_queue = new LinkedList<TreeNode>();
             node_queue.addLast(root);
-            node_queue.addLast(null);
+            node_queue.addLast(null);//每一层结束添加null结束标记
 
-            LinkedList<Integer> level_list = new LinkedList<Integer>();
-            boolean is_order_left = true;
+            //临时存储每一层的数据
+            LinkedList<Integer> level_list = new LinkedList<Integer>();//双向链表
+            boolean is_order_left = true; //每一层是否从左开始遍历，其实is_order_left也可以用level_list的个数是否是偶数来表示
 
             while (node_queue.size() > 0) {
+                /*每次都取第一个，同时删除第一个元素*/
                 TreeNode curr_node = node_queue.pollFirst();
-                if (curr_node != null) {
+                if (curr_node != null) {//证明这一层还没结束
                     if (is_order_left)
+                        //每一层是否从左开始遍历 方法同add
                         level_list.addLast(curr_node.val);
                     else
+                        //都加在第一个位置比如加入 1，2，3时 最后就成了 3，2，1
                         level_list.addFirst(curr_node.val);
 
+                    /*从左到右依次加入左子树和右子树*/
                     if (curr_node.left != null)
                         node_queue.addLast(curr_node.left);
                     if (curr_node.right != null)
                         node_queue.addLast(curr_node.right);
 
-                } else {
-                    // we finish the scan of one level
+                } else {//这一层要结束了
+                    //一层被遍历后
                     results.add(level_list);
                     level_list = new LinkedList<Integer>();
-                    // prepare for the next level
+                    //每一层结束添加null结束标记
                     if (node_queue.size() > 0)
                         node_queue.addLast(null);
+                    //每一层结束后开始变换顺序
                     is_order_left = !is_order_left;
                 }
             }
@@ -112,14 +118,16 @@ class Interview32_03 {
      */
     static class Solution2 {
         protected void DFS(TreeNode node, int level, List<List<Integer>> results) {
-            if (level >= results.size()) {
+            if (level >= results.size()) {/*更深的一层*/
                 LinkedList<Integer> newLevel = new LinkedList<Integer>();
                 newLevel.add(node.val);
-                results.add(newLevel);
+                results.add(newLevel);/*添加新的一层*/
             } else {
                 if (level % 2 == 0)
+                    //每一层是否从左开始遍历 方法同add
                     results.get(level).add(node.val);
                 else
+                    //都加在第一个位置比如加入 1，2，3时 最后就成了 3，2，1
                     results.get(level).add(0, node.val);
             }
 
@@ -136,5 +144,72 @@ class Interview32_03 {
             return results;
         }
     }
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
