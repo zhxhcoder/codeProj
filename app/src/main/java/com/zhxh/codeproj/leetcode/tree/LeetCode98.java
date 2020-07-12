17fc04b1900c93d1947a1254a5de73b0345e477a
zhxh <zhxhcoder@gmail.com>
2020-07-12 12:19:15 +0800

    验证二叉排序树

--- app/src/main/java/com/zhxh/codeproj/leetcode/tree/LeetCode98.java
+++ app/src/main/java/com/zhxh/codeproj/leetcode/tree/LeetCode98.java
@@ -1,5 +1,7 @@
 package com.zhxh.codeproj.leetcode.tree;
 
+import java.util.Stack;
+
 /*
 给定一个二叉树，判断其是否是一个有效的二叉搜索树。
 
@@ -32,6 +34,7 @@ class LeetCode98 {
     public static void main(String[] args) {
         TreeNode node = TreeNode.buildBinaryTree(new Integer[]{2, 1, 5, null, 5});
         System.out.println(new Solution().isValidBST(node));
+        System.out.println(new Solution1().isValidBST(node));
     }
 
     /*
@@ -57,4 +60,34 @@ class LeetCode98 {
         }
     }
 
+    /*
+    方法二：中序遍历
+ 思路和算法
+
+ 基于方法一中提及的性质，我们可以进一步知道二叉搜索树「中序遍历」得到的值构成的序列一定是升序的，这启示我们在中序遍历的时候实时检查当前节点的值是否大于前一个中序遍历到的节点的值即可。如果均大于说明这个序列是升序的，整棵树是二叉搜索树，
+ 否则不是，下面的代码我们使用栈来模拟中序遍历的过程。
+
+ 可能由读者不知道中序遍历是什么，我们这里简单提及一下，
+ 中序遍历是二叉树的一种遍历方式，它先遍历左子树，再遍历根节点，最后遍历右子树。而我们二叉搜索树保证了左子树的节点的值均小于根节点的值，根节点的值均小于右子树的值，因此中序遍历以后得到的序列一定是升序序列。
+
+     */
+    static class Solution1 {
+        public boolean isValidBST(TreeNode root) {
+            Stack<TreeNode> stack = new Stack();
+            double inorder = -Double.MAX_VALUE;
+
+            while (!stack.isEmpty() || root != null) {
+                while (root != null) {
+                    stack.push(root);
+                    root = root.left;
+                }
+                root = stack.pop();
+                // 如果中序遍历得到的节点的值小于等于前一个 inorder，说明不是二叉搜索树
+                if (root.val <= inorder) return false;
+                inorder = root.val;
+                root = root.right;
+            }
+            return true;
+        }
+    }
 }
