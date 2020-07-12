623181668f28fd263cccafff1f6746f5eb5cae5c
zhxh <zhxhcoder@gmail.com>
2020-07-12 12:26:59 +0800

    对称树

--- app/src/main/java/com/zhxh/codeproj/leetcode/tree/LeetCode101.java
+++ app/src/main/java/com/zhxh/codeproj/leetcode/tree/LeetCode101.java
@@ -1,5 +1,8 @@
 package com.zhxh.codeproj.leetcode.tree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /*
 
 给定一个二叉树，检查它是否是镜像对称的。
@@ -31,6 +34,7 @@ package com.zhxh.codeproj.leetcode.tree;
 public class LeetCode101 {
     public static void main(String[] args) {
         Solution solution = new Solution();
+        Solution1 solution1 = new Solution1();
 
         TreeNode node0 = TreeNode.buildBinaryTree(new Integer[]{1, 2, 2, null, 3, null, 3});
         TreeNode node1 = TreeNode.buildBinaryTree(new Integer[]{1, 2, 2, 3, 4, 4, 3});
@@ -38,8 +42,13 @@ public class LeetCode101 {
         System.out.println(solution.isSymmetric(node0));
         System.out.println(solution.isSymmetric(node1));
 
+        System.out.println(solution1.isSymmetric(node0));
+        System.out.println(solution1.isSymmetric(node1));
     }
 
+    /*
+    递归
+     */
     static class Solution {
         public boolean isSymmetric(TreeNode root) {
             return isMirror(root, root);
@@ -54,4 +63,35 @@ public class LeetCode101 {
         }
     }
 
+    /*
+    迭代
+     */
+    static class Solution1 {
+        public boolean isSymmetric(TreeNode root) {
+            return check(root, root);
+        }
+
+        public boolean check(TreeNode u, TreeNode v) {
+            Queue<TreeNode> q = new LinkedList<TreeNode>();
+            q.offer(u);
+            q.offer(v);
+            while (!q.isEmpty()) {
+                u = q.poll();
+                v = q.poll();
+                if (u == null && v == null) {
+                    continue;
+                }
+                if ((u == null || v == null) || (u.val != v.val)) {
+                    return false;
+                }
+
+                q.offer(u.left);
+                q.offer(v.right);
+
+                q.offer(u.right);
+                q.offer(v.left);
+            }
+            return true;
+        }
+    }
 }
