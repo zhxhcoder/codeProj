81781a2e8225f64bc841315c2bdad7728fdef1b4
zhxh <zhxhcoder@gmail.com>
2020-07-15 17:50:27 +0800

    滑动窗口

--- app/src/main/java/com/zhxh/codeproj/leetcode/array/LeetCode674.java
+++ app/src/main/java/com/zhxh/codeproj/leetcode/array/LeetCode674.java
@@ -26,12 +26,13 @@ class LeetCode674 {
 
     static class Solution {
         public int findLengthOfLCIS(int[] nums) {
-            int ans = 0, anchor = 0;
-            for (int i = 0; i < nums.length; ++i) {
-                if (i > 0 && nums[i - 1] >= nums[i]) anchor = i;
-                ans = Math.max(ans, i - anchor + 1);
+            int res = 0, anchor = 0;
+            for (int i = 0; i < nums.length; i++) {
+                if (i > 0 && nums[i - 1] >= nums[i])
+                    anchor = i;
+                res = Math.max(res, i - anchor + 1);
             }
-            return ans;
+            return res;
         }
     }
 }
