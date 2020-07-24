afcc58f94e9c0f87b5000660b6fe33df9ab94ab4
zhxh <zhxhcoder@gmail.com>
2020-07-24 14:50:18 +0800

    滑动窗口

--- app/src/main/java/com/zhxh/codeproj/leetcode/tree/LeetCode543.java
+++ app/src/main/java/com/zhxh/codeproj/leetcode/tree/LeetCode543.java
@@ -21,17 +21,14 @@ package com.zhxh.codeproj.leetcode.tree;
 
  */
 public class LeetCode543 {
-
     public static void main(String[] args) {
         Solution solution = new Solution();
 
         TreeNode node0 = TreeNode.buildBinaryTree(new Integer[]{1, 2, 2, null, 3, null, 3});
 
-
         System.out.println(solution.depth(node0));
         System.out.println(solution.diameterOfBinaryTree(node0));
     }
-
     static class Solution {
         int ans;
 
