910bf125cbe1fd30eb27571ce5ed2a1d1da3762d
zhxh <zhxhcoder@gmail.com>
2022-08-12 15:59:18 +0800

    前缀思想-注释

--- app/src/main/java/com/zhxh/codeproj/leetcode/LeetCode560.java
+++ app/src/main/java/com/zhxh/codeproj/leetcode/LeetCode560.java
@@ -78,22 +78,30 @@ class LeetCode560 {
             if (nums.length == 0) {
                 return 0;
             }
-            HashMap<Integer, Integer> map = new HashMap<>();
-            //细节，这里需要预存前缀和为 0 的情况，会漏掉前几位就满足的情况
-            //例如输入[1,1,0]，k = 2 如果没有这行代码，则会返回0,漏掉了1+1=2，和1+1+0=2的情况
-            //输入：[3,1,1,0] k = 2时则不会漏掉
-            //因为presum[3] - presum[0]表示前面 3 位的和，所以需要map.put(0,1),垫下底
-            map.put(0, 1);
+            // hash
+            // 记录合适的连续字符串数量
             int count = 0;
-            int presum = 0;
-            for (int x : nums) {
-                presum += x;
-                //当前前缀和已知，判断是否含有 presum - k的前缀和，那么我们就知道某一区间的和为 k 了。
-                if (map.containsKey(presum - k)) {
-                    count += map.get(presum - k);//获取次数
+            // 记录前面数字相加之和
+            int pre = 0;
+            // map记录前几个数字之和为K出现相同和的次数为V
+            HashMap<Integer, Integer> map = new HashMap<>();
+            // 初始化
+            map.put(0, 1);
+            for (int i = 0; i < nums.length; i++) {
+                pre += nums[i];
+                // 前缀和
+                // 设
+                // pre[i]=pre[i−1]+nums[i]
+                // 由于补上了0，1这个情况 问题由多少个连续数字之和等于k 转为
+                // pre[i]−pre[j−1]==k （前缀和之差为k，代表这两个前缀和中间的数字相加就是K）
+                // 如果前面某些数字之和加上这个数字正好等于K（存在一个数字加上nums[i]结果为K
+                // 说明找到了
+                if (map.containsKey(pre - k)) {
+                    // 累计
+                    count += map.get(pre - k);
                 }
-                //更新
-                map.put(presum, map.getOrDefault(presum, 0) + 1);
+                // 计算新的和放入map
+                map.put(pre, map.getOrDefault(pre, 0) + 1);
             }
             return count;
         }
