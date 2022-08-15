4afc43f5ca3df098ff084b21a37587c3f650e3ab
zhxh <zhxhcoder@gmail.com>
2022-08-15 16:02:28 +0800

    hashMap遍历

--- app/src/main/java/com/zhxh/codeproj/leetcode/array/LeetCode01.java
+++ app/src/main/java/com/zhxh/codeproj/leetcode/array/LeetCode01.java
@@ -88,9 +88,9 @@ public class LeetCode01 {
         public int[] twoSum(int[] nums, int target) {
             Map<Integer, Integer> map = new HashMap<>();
             for (int i = 0; i < nums.length; i++) {
-                int temp = target - nums[i];
-                if (map.containsKey(temp)) {
-                    return new int[]{i, map.get(temp)};
+                int key = target - nums[i];
+                if (map.containsKey(key)) {
+                    return new int[]{i, map.get(key)};
                 }
                 map.put(nums[i], i);
             }
