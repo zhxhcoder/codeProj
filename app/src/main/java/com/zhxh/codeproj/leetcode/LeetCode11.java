299abb578ba72192f7bbd34d61e5a6adfe9744b5
zhxh <zhxhcoder@gmail.com>
2022-08-16 13:39:03 +0800

    水容器

--- app/src/main/java/com/zhxh/codeproj/leetcode/LeetCode11.java
+++ app/src/main/java/com/zhxh/codeproj/leetcode/LeetCode11.java
@@ -3,18 +3,52 @@ package com.zhxh.codeproj.leetcode;
 /*
 给你 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点(i,ai) 。在坐标内画 n 条垂直线，垂直线 i的两个端点分别为(i,ai) 和 (i, 0)。找出其中的两条线，使得它们与x轴共同构成的容器可以容纳最多的水。
 说明：你不能倾斜容器，且n的值至少为 2
+示例 1：
+
+
+
+输入：[1,8,6,2,5,4,8,3,7]
+输出：49 
+解释：图中垂直线代表输入数组 [1,8,6,2,5,4,8,3,7]。在此情况下，容器能够容纳水（表示为蓝色部分）的最大值为49。
+示例 2：
+
+输入：height = [1,1]
+输出：1
+
+
+提示：
+
+n == height.length
+2 <= n <= 105
+0 <= height[i] <= 104
 
  */
 public class LeetCode11 {
     public static void main(String[] args) {
-        int[] height = {8, 10, 2, 2, 2, 2, 2, 99, 9};
-        Solution solution = new Solution();
-        int maxArea = solution.maxArea(height);
-        System.out.println("maxArea:" + maxArea);
+        System.out.println(new Solution().maxArea(new int[]{8, 10, 2, 2, 2, 2, 2, 99, 9}));
+        System.out.println(new Solution().maxArea1(new int[]{8, 10, 2, 2, 2, 2, 2, 99, 9}));
     }
 
+
     public static class Solution {
+        /*
+        暴力解法
+         */
         public int maxArea(int[] height) {
+            int maxarea = 0;
+            for (int i = 0; i < height.length; i++) {
+                for (int j = i + 1; j < height.length; j++) {
+                    maxarea = Math.max(maxarea, Math.min(height[i], height[j]) * (j - i));
+                }
+            }
+            return maxarea;
+        }
+
+
+        /*
+        双指针
+         */
+        public int maxArea1(int[] height) {
             int maxarea = 0, l = 0, r = height.length - 1;
             while (l < r) {
                 maxarea = Math.max(maxarea, Math.min(height[l], height[r]) * (r - l));
