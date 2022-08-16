555cf8a4c328dcb1262d8761bb9f65e15fc5840e
zhxh <zhxhcoder@gmail.com>
2022-08-16 11:46:33 +0800

    最长子串问题-动态规划

---
+++ app/src/main/java/com/zhxh/codeproj/leetcode/dynamicplan/LeetCode10.java
@@ -0,0 +1,65 @@
+package com.zhxh.codeproj.leetcode.dynamicplan;
+
+/*
+给你一个字符串s和一个字符规律p，请你来实现一个支持 '.'和'*'的正则表达式匹配。
+
+'.' 匹配任意单个字符
+'*' 匹配零个或多个前面的那一个元素
+所谓匹配，是要涵盖整个字符串s的，而不是部分字符串。
+
+
+示例 1：
+
+输入：s = "aa", p = "a"
+输出：false
+解释："a" 无法匹配 "aa" 整个字符串。
+示例 2:
+
+输入：s = "aa", p = "a*"
+输出：true
+解释：因为 '*' 代表可以匹配零个或多个前面的那一个元素, 在这里前面的元素就是 'a'。因此，字符串 "aa" 可被视为 'a' 重复了一次。
+示例3：
+
+输入：s = "ab", p = ".*"
+输出：true
+解释：".*" 表示可匹配零个或多个（'*'）任意字符（'.'）。
+
+
+提示：
+
+1 <= s.length<= 20
+1 <= p.length<= 30
+s只包含从a-z的小写字母。
+p只包含从a-z的小写字母，以及字符.和*。
+保证每次出现字符* 时，前面都匹配到有效的字符
+
+ */
+public class LeetCode10 {
+    public static void main(String[] args) {
+        System.out.println(new Solution().isMatch("aaabaaa", "a*"));
+    }
+
+    static class Solution {
+        /*
+        动态规划
+         */
+        public boolean isMatch(String text, String pattern) {
+            boolean[][] dp = new boolean[text.length() + 1][pattern.length() + 1];
+            dp[text.length()][pattern.length()] = true;
+
+            for (int i = text.length(); i >= 0; i--) {
+                for (int j = pattern.length() - 1; j >= 0; j--) {
+                    boolean first_match = (i < text.length() &&
+                            (pattern.charAt(j) == text.charAt(i) ||
+                                    pattern.charAt(j) == '.'));
+                    if (j + 1 < pattern.length() && pattern.charAt(j + 1) == '*') {
+                        dp[i][j] = dp[i][j + 2] || first_match && dp[i + 1][j];
+                    } else {
+                        dp[i][j] = first_match && dp[i + 1][j + 1];
+                    }
+                }
+            }
+            return dp[0][0];
+        }
+    }
+}
