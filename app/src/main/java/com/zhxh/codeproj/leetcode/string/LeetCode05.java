aa428ec0344c9208969130e1ae0f1abbdd30b065
zhxh <zhxhcoder@gmail.com>
2022-08-16 11:09:04 +0800

    最长子串问题

--- app/src/main/java/com/zhxh/codeproj/leetcode/string/LeetCode05.java
+++ app/src/main/java/com/zhxh/codeproj/leetcode/string/LeetCode05.java
@@ -15,17 +15,63 @@ s 仅由数字和英文字母组成
  */
 public class LeetCode05 {
     public static void main(String[] args) {
-        System.out.println(new Solution().longestPalindrome("babad"));
+        System.out.println(new Solution().longestPalindrome("babcbad"));
+        System.out.println(new Solution().longestPalindrome2("babcbad"));
     }
 
     static class Solution {
+        /*
+        暴力解法
+         */
         public String longestPalindrome(String s) {
+            int len = s.length();
+            if (len < 2) {
+                return s;
+            }
+            int maxLen = 1;
+            int begin = 0;
+            //s.charAt(i)每次都会检查数组下标越界，因此先转换成字符数组，这一步非必需
+            char[] charArray = s.toCharArray();
+
+            //枚举所有长度严格大约1的子串 charArray[i..j]
+            for (int i = 0; i < len - 1; i++) {
+                for (int j = i + 1; j < len; j++) {
+                    if (j - i + 1 > maxLen && validPalindromic(charArray, i, j)) {
+                        maxLen = j - i + 1;
+                        begin = i;
+                    }
+                }
+            }
+
+            return s.substring(begin, begin + maxLen);
+
+
+        }
+
+        private boolean validPalindromic(char[] charArray, int left, int right) {
+            while (left < right) {
+                if (charArray[left] != charArray[right]) {
+                    return false;
+                }
+                left++;
+                right--;
+            }
+            return true;
+        }
+
+        /*
+        中心扩散法
+         - 枚举所有可能的回文子串的中心位置；
+         - 中心位置可能是一个字符，也有可能是两个相邻的字符
+         - 记录最长回文子串的相关变量
+         */
+        public String longestPalindrome2(String s) {
             if (s == null || s.length() < 1) return "";
             int start = 0, end = 0;
             for (int i = 0; i < s.length(); i++) {
-                int len1 = expandAroundCenter(s, i, i);
-                int len2 = expandAroundCenter(s, i, i + 1);
-                int len = Math.max(len1, len2);
+                int oddLen = expandAroundCenter(s, i, i);
+                int evenLen = expandAroundCenter(s, i, i + 1);
+                int len = Math.max(oddLen, evenLen);
                 if (len > end - start) {
                     start = i - (len - 1) / 2;
                     end = i + len / 2;
