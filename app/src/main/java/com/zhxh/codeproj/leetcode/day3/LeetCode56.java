cdf96626fb92c31d6adb98a4ad1652dc22f933f4
zhxh <zhxhcoder@gmail.com>
2022-08-17 18:59:15 +0800

    day3-合并区间

--- app/src/main/java/com/zhxh/codeproj/leetcode/day3/LeetCode56.java
+++ app/src/main/java/com/zhxh/codeproj/leetcode/day3/LeetCode56.java
@@ -32,15 +32,13 @@ public class LeetCode56 {
     }
 
     static class Solution {
-        private class IntervalComparator implements Comparator<Interval> {
-            @Override
-            public int compare(Interval a, Interval b) {
-                return a.start < b.start ? -1 : a.start == b.start ? 0 : 1;
-            }
-        }
-
         public List<Interval> merge(List<Interval> intervals) {
-            Collections.sort(intervals, new IntervalComparator());
+            Collections.sort(intervals, new Comparator<Interval>() {
+                @Override
+                public int compare(Interval a, Interval b) {
+                    return a.start < b.start ? -1 : a.start == b.start ? 0 : 1;
+                }
+            });
 
             LinkedList<Interval> merged = new LinkedList<Interval>();
             for (Interval interval : intervals) {
--- app/src/main/java/com/zhxh/codeproj/leetcode/day3/LeetCode64.java
+++ app/src/main/java/com/zhxh/codeproj/leetcode/day3/LeetCode64.java
@@ -24,8 +24,25 @@ public class LeetCode64 {
                 {1, 5, 1},
                 {4, 2, 1}
         }));
+        System.out.println(new Solution().minPathSum2(new int[][]{
+                {1, 3, 1},
+                {1, 5, 1},
+                {4, 2, 1}
+        }));
     }
 
+    /*
+    思路分析：
+    -很明显，我们在遇到这样的统计可行路径的数量，或者求最小路径的时候，比较容易想到的两种做法，一个是搜索，另一个是动态规划
+    -而搜索的做法，仅仅在数据规模比较小的时候才考虑使用，所以在这里，我们尝试采用动态规划来解决这个问题。
+
+    动态规划，主要关注以下两点
+    1，状态的设置。在这个题目里，由于要求最小路径和，我们可以令dp[i][j]代表从（i,j）走到（m-1,n-1）点的最小路径和
+    2，状态转移方程。我们考虑如何来求出dp[i][j]。由于每次只能往右或下走，所以(i,j)只能走到(i+1,j)或者(i,j+1)。
+       换言之，dp[i][j]的前续状态只有dp[i+1][j],dp[i][j+1],所以我们在两者取最小，然后加上这个格子内的数即可。
+       dp(i,j)=grid(i,j)+min(dp(i+1,j),dp(i,j+1))
+
+     */
     static class Solution {
         public int minPathSum(int[][] grid) {
             int[][] dp = new int[grid.length][grid[0].length];
@@ -42,6 +59,30 @@ public class LeetCode64 {
                 }
             }
             return dp[0][0];
+        }
+
+        /*
+        动态规划-优化方法滚动数组
+        我们可以用一个一维数组来代替二维数组，dp数组的大小和行大小相同。
+        这是因为对于某个固定状态，只需要考虑下方和右侧的节点。
+        我们就可以一行一行计算，来节省空间复杂度。
+         */
+        public int minPathSum2(int[][] grid) {
+            int[] dp = new int[grid[0].length];
+            for (int i = grid.length - 1; i >= 0; i--) {
+                for (int j = grid[0].length - 1; j >= 0; j--) {
+                    if (i == grid.length - 1 && j != grid[0].length - 1) {
+                        dp[j] = grid[i][j] + dp[j + 1];
+                    } else if (j == grid[0].length - 1 && i != grid.length - 1) {
+                        dp[j] = grid[i][j] + dp[j];
+                    } else if (j != grid[0].length - 1 && i != grid.length - 1) {
+                        dp[j] = grid[i][j] + Math.min(dp[j], dp[j + 1]);
+                    } else {
+                        dp[j] = grid[i][j];
+                    }
+                }
+            }
+            return dp[0];
         }
     }
 }
