4ad8847304ae484748752b62508f5dda2f56b054
zhxh <zhxhcoder@gmail.com>
2022-08-18 10:09:46 +0800

    day4-最小覆盖子串

--- app/src/main/java/com/zhxh/codeproj/leetcode/day4/LeetCode75.java
+++ app/src/main/java/com/zhxh/codeproj/leetcode/day4/LeetCode75.java
@@ -23,22 +23,24 @@ import java.util.Arrays;
  */
 class LeetCode75 {
     public static void main(String[] args) {
-        int[] nums = {2, 0, 2, 1, 1, 0};
-        new Solution().sortColors(nums);
-        System.out.println(Arrays.toString(nums));
+        int[] nums1 = {2, 0, 2, 1, 1, 0};
+        new Solution().sortColors(nums1);
+        System.out.println(Arrays.toString(nums1));
+
+        int[] nums2 = {2, 0, 2, 1, 1, 0};
+        new Solution().sortColors2(nums2);
+        System.out.println(Arrays.toString(nums2));
     }
 
     /*
-    本问题被称为 荷兰国旗问题
-，最初由 Edsger W. Dijkstra提出。
-其主要思想是给每个数字设定一种颜色，并按照荷兰国旗颜色的顺序进行调整。
-
-我们用三个指针（p0, p2 和curr）来分别追踪0的最右边界，2的最左边界和当前考虑的元素。
-
+    本问题被称为 荷兰国旗问题，最初由 Edsger W. Dijkstra提出。
+    其主要思想是给每个数字设定一种颜色，并按照荷兰国旗颜色的顺序进行调整。
+    我们用三个指针（p0, p2 和curr）来分别追踪0的最右边界，2的最左边界和当前考虑的元素。
      */
     static class Solution {
         /*
         荷兰三色旗问题解
+        双指针算法
         */
         public void sortColors(int[] nums) {
             // 对于所有 idx < i : nums[idx < i] = 0
@@ -63,6 +65,48 @@ class LeetCode75 {
                     nums[p2--] = tmp;
                 } else curr++;
             }
+        }
+
+        /*
+        快速排序算法
+        思路：借助快速排序partition过程的一趟扫描法
+        -回顾快速排序partition过程：随机选择一个元素作为切分元素（pivot），然后经过一次扫描，通过交换不同位置的元素使得数组按照数值大小分成以下3个部分：
+         <pivot  ==pivot  >pivot
+
+         循环不变量 分区定义：
+         [0,p0) ==0
+         [p0,i) ==1
+         (p2,len01] ==2
+
+         */
+        public void sortColors2(int[] nums) {
+            int len = nums.length;
+            if (len < 2) {
+                return;
+            }
+
+            int p0 = 0;
+            int i = 0;
+            int p2 = len - 1;
+            while (i <= p2) {//(p2,len01] ==2
+                if (nums[i] == 0) {
+                    swap(nums, i, p0);
+                    p0++;
+                    i++;
+                } else if (nums[i] == 1) {
+                    i++;
+                } else {
+                    //nums[i] ==2
+                    swap(nums, i, p2);
+                    p2--;
+                }
+            }
+        }
+
+        private void swap(int[] nums, int i, int j) {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
         }
     }
 }
--- app/src/main/java/com/zhxh/codeproj/leetcode/day4/LeetCode76.java
+++ app/src/main/java/com/zhxh/codeproj/leetcode/day4/LeetCode76.java
@@ -1,7 +1,90 @@
 package com.zhxh.codeproj.leetcode.day4;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+/*
+给你一个字符串 s 、一个字符串 t 。返回 s 中涵盖 t 所有字符的最小子串。如果 s 中不存在涵盖 t 所有字符的子串，则返回空字符串 "" 。
+
+注意：
+
+对于 t 中重复字符，我们寻找的子字符串中该字符数量必须不少于 t 中该字符数量。
+如果 s 中存在这样的子串，我们保证它是唯一的答案。
+
+示例 1：
+
+输入：s = "ADOBECODEBANC", t = "ABC"
+输出："BANC"
+示例 2：
+
+输入：s = "a", t = "a"
+输出："a"
+示例 3:
+
+输入: s = "a", t = "aa"
+输出: ""
+解释: t 中两个字符 'a' 均应包含在 s 的子串中，
+因此没有符合条件的子字符串，返回空字符串。
+
+
+提示：
+
+1 <= s.length, t.length <= 105
+s 和 t 由英文字母组成
+
+
+进阶：你能设计一个在 o(n) 时间内解决此问题的算法吗？
+ */
 public class LeetCode76 {
     public static void main(String[] args) {
+        System.out.println(new Solution().minWindow("ADOBECODEBANC", "ABC"));
+    }
 
+    static class Solution {
+        Map<Character, Integer> ori = new HashMap<Character, Integer>();
+        Map<Character, Integer> cnt = new HashMap<Character, Integer>();
+
+        public String minWindow(String s, String t) {
+            int tLen = t.length();
+            for (int i = 0; i < tLen; i++) {
+                char c = t.charAt(i);
+                ori.put(c, ori.getOrDefault(c, 0) + 1);
+            }
+            int l = 0, r = -1;
+            int len = Integer.MAX_VALUE, ansL = -1, ansR = -1;
+            int sLen = s.length();
+            while (r < sLen) {
+                ++r;
+                if (r < sLen && ori.containsKey(s.charAt(r))) {
+                    cnt.put(s.charAt(r), cnt.getOrDefault(s.charAt(r), 0) + 1);
+                }
+                while (check() && l <= r) {
+                    if (r - l + 1 < len) {
+                        len = r - l + 1;
+                        ansL = l;
+                        ansR = l + len;
+                    }
+                    if (ori.containsKey(s.charAt(l))) {
+                        cnt.put(s.charAt(l), cnt.getOrDefault(s.charAt(l), 0) - 1);
+                    }
+                    ++l;
+                }
+            }
+            return ansL == -1 ? "" : s.substring(ansL, ansR);
+        }
+
+        public boolean check() {
+            Iterator iter = ori.entrySet().iterator();
+            while (iter.hasNext()) {
+                Map.Entry entry = (Map.Entry) iter.next();
+                Character key = (Character) entry.getKey();
+                Integer val = (Integer) entry.getValue();
+                if (cnt.getOrDefault(key, 0) < val) {
+                    return false;
+                }
+            }
+            return true;
+        }
     }
 }
