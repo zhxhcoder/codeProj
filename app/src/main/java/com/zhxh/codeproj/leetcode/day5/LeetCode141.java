381e1e4d0e956bf2f2ba407999c00ea955bbebe5
zhxh <zhxhcoder@gmail.com>
2022-08-19 10:38:45 +0800

    day5-动态规划

--- app/src/main/java/com/zhxh/codeproj/leetcode/day5/LeetCode141.java
+++ app/src/main/java/com/zhxh/codeproj/leetcode/day5/LeetCode141.java
@@ -35,19 +35,14 @@ import java.util.Set;
 
  */
 public class LeetCode141 {
-
     public static void main(String[] args) {
-        Solution solution = new Solution();
-        ListNode node1 = new ListNode(3);
-
-
+        System.out.println(new Solution().hasCycle(ListNode.buildNode(new int[]{3, 2, 0, -4})));
     }
 
-    /*
-    我们设置快慢两个指针，快指针每次走2步，慢指针每次走1步
-如视频所示，跑道是链表，兔子（快指针）每次走2步，乌龟（慢指针）每次走1步，在走完跑道（链表）的前提下，如果兔子和乌龟（快慢指针）相遇，说明跑道（链表）有环，否则说明跑道没有环。
-     */
     static class Solution {
+        /*
+        哈希表
+         */
         public boolean hasCycle(ListNode head) {
             Set<ListNode> nodesSeen = new HashSet<>();
             while (head != null) {
@@ -59,6 +54,26 @@ public class LeetCode141 {
                 head = head.next;
             }
             return false;
+        }
+
+        /*
+         我们设置快慢两个指针，快指针每次走2步，慢指针每次走1步
+         如视频所示，跑道是链表，兔子（快指针）每次走2步，乌龟（慢指针）每次走1步，在走完跑道（链表）的前提下，如果兔子和乌龟（快慢指针）相遇，说明跑道（链表）有环，否则说明跑道没有环。
+        */
+        public boolean hasCycle2(ListNode head) {
+            if (head == null || head.next == null) {
+                return false;
+            }
+            ListNode slow = head;
+            ListNode fast = head.next;
+            while (slow != fast) {
+                if (fast == null || fast.next == null) {
+                    return false;
+                }
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+            return true;
         }
     }
 }
