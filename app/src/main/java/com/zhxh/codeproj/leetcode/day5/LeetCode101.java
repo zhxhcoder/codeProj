ba21beee89ef1a2c064c6a176ecc3bd7fb513e29
zhxh <zhxhcoder@gmail.com>
2022-08-19 09:46:49 +0800

    day4-回溯算法

--- app/src/main/java/com/zhxh/codeproj/leetcode/day5/LeetCode101.java
+++ app/src/main/java/com/zhxh/codeproj/leetcode/day5/LeetCode101.java
@@ -38,12 +38,17 @@ public class LeetCode101 {
         System.out.println(new Solution().isSymmetric(TreeNode.buildBinaryTree(new Integer[]{1, 2, 2, null, 3, null, 3})));
         System.out.println(new Solution().isSymmetric(TreeNode.buildBinaryTree(new Integer[]{1, 2, 2, 3, 4, 4, 3})));
 
-        System.out.println(new Solution1().isSymmetric(TreeNode.buildBinaryTree(new Integer[]{1, 2, 2, null, 3, null, 3})));
-        System.out.println(new Solution1().isSymmetric(TreeNode.buildBinaryTree(new Integer[]{1, 2, 2, 3, 4, 4, 3})));
+        System.out.println(new Solution2().isSymmetric(TreeNode.buildBinaryTree(new Integer[]{1, 2, 2, null, 3, null, 3})));
+        System.out.println(new Solution2().isSymmetric(TreeNode.buildBinaryTree(new Integer[]{1, 2, 2, 3, 4, 4, 3})));
     }
 
     /*
     递归
+    如果一个树的左子树与右子树镜像对称，那么这个树是对称的
+    因此，该问题可以转化为：两个树在什么情况下互为镜像
+    如果同时满足下面的条件，两个树互为镜像：
+    - 他们的两个根节点具有相同的值
+    - 每个树的右子树都与另一个树的左子树镜像对称
      */
     static class Solution {
         public boolean isSymmetric(TreeNode root) {
@@ -61,8 +66,14 @@ public class LeetCode101 {
 
     /*
     迭代
+    方法一 中我们用递归的方法实现了对称性的判断，如何用迭代实现，首先我们引入一个队列，
+    这是把递归程序写成迭代程序的常用方法。初始化时我们把根节点入队两次。每次提取两个结点
+    并比较他们的值（队列中每两个连续的结点应该是相等的，而且它们的子树互为镜像），然后将两个结点
+    的左右字节点按相反的顺序插入队列中。
+    当队列为空时，或者我们检测不到树不对称（即从队列中取出两个不相等的连续结点）时，该算法结束。
+
      */
-    static class Solution1 {
+    static class Solution2 {
         public boolean isSymmetric(TreeNode root) {
             return check(root, root);
         }
