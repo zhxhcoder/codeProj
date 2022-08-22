a6cba8a1e7166a466d7741a991293052dbdc5f72
zhxh <zhxhcoder@gmail.com>
2022-08-22 14:00:51 +0800

    day8-二分查找

--- app/src/main/java/com/zhxh/codeproj/leetcode/day8/LeetCode297.java
+++ app/src/main/java/com/zhxh/codeproj/leetcode/day8/LeetCode297.java
@@ -1,5 +1,11 @@
 package com.zhxh.codeproj.leetcode.day8;
 
+import com.zhxh.codeproj.leetcode._base.TreeNode;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 /*
 序列化是将一个数据结构或者对象转换为连续的比特位的操作，进而可以将转换后的数据存储在一个文件或者内存中，同时也可以通过网络传输到另一个计算机环境，采取相反方式重构得到原数据。
 
@@ -36,6 +42,41 @@ package com.zhxh.codeproj.leetcode.day8;
  */
 public class LeetCode297 {
     public static void main(String[] args) {
+        TreeNode.prettyPrintTree(new Codec().deserialize("1,2,3,4,5"));
+    }
 
+    static class Codec {
+        public String serialize(TreeNode root) {
+            return rserialize(root, "");
+        }
+
+        public TreeNode deserialize(String data) {
+            String[] data_array = data.split(",");
+            List<String> data_list = new LinkedList<String>(Arrays.asList(data_array));
+            return rdeserialize(data_list);
+        }
+
+        public String rserialize(TreeNode root, String str) {
+            if (root == null) {
+                str += "None,";
+            } else {
+                str += str.valueOf(root.val) + ",";
+                str = rserialize(root.left, str);
+                str = rserialize(root.right, str);
+            }
+            return str;
+        }
+
+        public TreeNode rdeserialize(List<String> l) {
+            if (l.get(0).equals("None")) {
+                l.remove(0);
+                return null;
+            }
+            TreeNode root = new TreeNode(Integer.valueOf(l.get(0)));
+            l.remove(0);
+            root.left = rdeserialize(l);
+            root.right = rdeserialize(l);
+            return root;
+        }
     }
 }
