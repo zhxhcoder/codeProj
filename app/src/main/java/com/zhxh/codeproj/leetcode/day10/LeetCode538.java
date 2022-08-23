e7a02c675f09920665704222dc8f15a7b4b3c8e9
zhxh <zhxhcoder@gmail.com>
2022-08-23 14:44:19 +0800

    day9-二叉搜索树

--- app/src/main/java/com/zhxh/codeproj/leetcode/day10/LeetCode538.java
+++ app/src/main/java/com/zhxh/codeproj/leetcode/day10/LeetCode538.java
@@ -23,8 +23,15 @@ import com.zhxh.codeproj.leetcode._base.TreeNode;
 public class LeetCode538 {
     public static void main(String[] args) {
         TreeNode.prettyPrintTree(new Solution().convertBST(TreeNode.buildBinaryTree(new Integer[]{4, 1, 6, 0, 2, 5, 7, null, null, null, 3, null, null, null, 8})));
+        TreeNode.prettyPrintTree(new Solution2().convertBST(TreeNode.buildBinaryTree(new Integer[]{4, 1, 6, 0, 2, 5, 7, null, null, null, 3, null, null, null, 8})));
     }
 
+    /*
+    二叉搜索树的中序遍历是一个单调递增的有序序列。如果我们反序地中序遍历该二叉搜索树，即可得到一个单调的一个递减的有序序列。
+    反序中序遍历
+    本题中要求我们将每个节点的值修改为原来的节点值加上所有大于它的节点值之和。
+    这样我们只需要反序地中序遍历该二叉搜索树，即可得到一个单调递减的有序序列。
+     */
     static class Solution {
         private int sum = 0;
 
@@ -36,6 +43,46 @@ public class LeetCode538 {
                 convertBST(root.left);
             }
             return root;
+        }
+    }
+
+    /*
+    Morris 遍历
+    只占用常数空间来实现中序遍历。
+     */
+    static class Solution2 {
+        public TreeNode convertBST(TreeNode root) {
+            int sum = 0;
+            TreeNode node = root;
+
+            while (node != null) {
+                if (node.right == null) {
+                    sum += node.val;
+                    node.val = sum;
+                    node = node.left;
+                } else {
+                    TreeNode succ = getSuccessor(node);
+                    if (succ.left == null) {
+                        succ.left = node;
+                        node = node.right;
+                    } else {
+                        succ.left = null;
+                        sum += node.val;
+                        node.val = sum;
+                        node = node.left;
+                    }
+                }
+            }
+
+            return root;
+        }
+
+        public TreeNode getSuccessor(TreeNode node) {
+            TreeNode succ = node.right;
+            while (succ.left != null && succ.left != node) {
+                succ = succ.left;
+            }
+            return succ;
         }
     }
 }
