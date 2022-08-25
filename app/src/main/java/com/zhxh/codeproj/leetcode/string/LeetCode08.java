3c5397bc2d04b203b3e450b61c851d2c5199ed2c
zhxh <zhxhcoder@gmail.com>
2022-08-25 13:23:06 +0800

    day2-自动机

--- app/src/main/java/com/zhxh/codeproj/leetcode/string/LeetCode08.java
+++ app/src/main/java/com/zhxh/codeproj/leetcode/string/LeetCode08.java
@@ -57,6 +57,7 @@ class LeetCode08 {
         System.out.println(new Solution().myAtoi("  4193 with words"));
         System.out.println(new Solution2().myAtoi("  4193 with words"));
         System.out.println(new Solution3().myAtoi("  4193 with words"));
+        System.out.println(new Solution4().myAtoi("  4193 with words"));
     }
 
     /*
@@ -179,6 +180,55 @@ class LeetCode08 {
                 }
             }
             return res;
+        }
+    }
+
+    /*
+    官网提供的自动机
+     */
+    static class Solution4 {
+        public int myAtoi(String str) {
+            Automaton automaton = new Automaton();
+            int length = str.length();
+            for (int i = 0; i < length; ++i) {
+                automaton.get(str.charAt(i));
+            }
+            return (int) (automaton.sign * automaton.ans);
+        }
+    }
+
+    static class Automaton {
+        public int sign = 1;
+        public long ans = 0;
+        private String state = "start";
+        private Map<String, String[]> table = new HashMap<String, String[]>() {{
+            put("start", new String[]{"start", "signed", "in_number", "end"});
+            put("signed", new String[]{"end", "end", "in_number", "end"});
+            put("in_number", new String[]{"end", "end", "in_number", "end"});
+            put("end", new String[]{"end", "end", "end", "end"});
+        }};
+
+        public void get(char c) {
+            state = table.get(state)[get_col(c)];
+            if ("in_number".equals(state)) {
+                ans = ans * 10 + c - '0';
+                ans = sign == 1 ? Math.min(ans, (long) Integer.MAX_VALUE) : Math.min(ans, -(long) Integer.MIN_VALUE);
+            } else if ("signed".equals(state)) {
+                sign = c == '+' ? 1 : -1;
+            }
+        }
+
+        private int get_col(char c) {
+            if (c == ' ') {
+                return 0;
+            }
+            if (c == '+' || c == '-') {
+                return 1;
+            }
+            if (Character.isDigit(c)) {
+                return 2;
+            }
+            return 3;
         }
     }
 }
