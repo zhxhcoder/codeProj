3d3d7ca0ec6b3229db66dcc3d14b412beff40cec
zhxh <zhxhcoder@gmail.com>
2022-08-26 13:56:17 +0800

    中序遍历

--- app/src/main/java/com/zhxh/codeproj/leetcode/_tree/LeetCode108.java
+++ app/src/main/java/com/zhxh/codeproj/leetcode/_tree/LeetCode108.java
@@ -3,6 +3,8 @@ package com.zhxh.codeproj.leetcode._tree;
 
 import com.zhxh.codeproj.leetcode.__base.TreeNode;
 
+import java.util.Random;
+
 /*
 将一个按照升序排列的有序数组，转换为一棵高度平衡二叉搜索树。
 
@@ -23,33 +25,78 @@ import com.zhxh.codeproj.leetcode.__base.TreeNode;
  */
 class LeetCode108 {
     public static void main(String[] args) {
-
+        TreeNode.prettyPrintTree(new Solution().sortedArrayToBST(new int[]{-10, -3, 0, 5, 9}));
+        TreeNode.prettyPrintTree(new Solution2().sortedArrayToBST(new int[]{-10, -3, 0, 5, 9}));
+        TreeNode.prettyPrintTree(new Solution3().sortedArrayToBST(new int[]{-10, -3, 0, 5, 9}));
     }
 
     /*
-    方法二：中序遍历：始终选择中间位置右边元素作为根节点
+    方法一：中序遍历，总是选择中间位置左边的数字作为根节点
      */
 
-    class Solution {
-        int[] nums;
-
-        public TreeNode helper(int left, int right) {
-            if (left > right) return null;
-
-            // always choose right middle node as a root
-            int p = (left + right) / 2;
-            if ((left + right) % 2 == 1) ++p;
-
-            // inorder traversal: left -> node -> right
-            TreeNode root = new TreeNode(nums[p]);
-            root.left = helper(left, p - 1);
-            root.right = helper(p + 1, right);
-            return root;
+    static class Solution {
+        public TreeNode sortedArrayToBST(int[] nums) {
+            return helper(nums, 0, nums.length - 1);
         }
 
+        public TreeNode helper(int[] nums, int left, int right) {
+            if (left > right) {
+                return null;
+            }
+            // 总是选择中间位置左边的数字作为根节点
+            int mid = (left + right) / 2;
+
+            TreeNode root = new TreeNode(nums[mid]);
+            root.left = helper(nums, left, mid - 1);
+            root.right = helper(nums, mid + 1, right);
+            return root;
+        }
+    }
+
+    /*
+    方法二：中序遍历，总是选择中间位置右边的数字作为根节点
+     */
+    static class Solution2 {
         public TreeNode sortedArrayToBST(int[] nums) {
-            this.nums = nums;
-            return helper(0, nums.length - 1);
+            return helper(nums, 0, nums.length - 1);
+        }
+
+        public TreeNode helper(int[] nums, int left, int right) {
+            if (left > right) {
+                return null;
+            }
+
+            // 总是选择中间位置右边的数字作为根节点
+            int mid = (left + right + 1) / 2;
+
+            TreeNode root = new TreeNode(nums[mid]);
+            root.left = helper(nums, left, mid - 1);
+            root.right = helper(nums, mid + 1, right);
+            return root;
+        }
+    }
+
+    /*
+    方法三：中序遍历，选择任意一个中间位置数字作为根节点
+     */
+    static class Solution3 {
+        Random rand = new Random();
+
+        public TreeNode sortedArrayToBST(int[] nums) {
+            return helper(nums, 0, nums.length - 1);
+        }
+
+        public TreeNode helper(int[] nums, int left, int right) {
+            if (left > right) {
+                return null;
+            }
+            // 选择任意一个中间位置数字作为根节点
+            int mid = (left + right + rand.nextInt(2)) / 2;
+
+            TreeNode root = new TreeNode(nums[mid]);
+            root.left = helper(nums, left, mid - 1);
+            root.right = helper(nums, mid + 1, right);
+            return root;
         }
     }
 }
