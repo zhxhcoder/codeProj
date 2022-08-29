c971c85951f9960477698b1bb297658bc8c72128
zhxh <zhxhcoder@gmail.com>
2022-08-29 15:34:17 +0800

    使用 volatile 关键字修饰 变量的方式终止

---
+++ app/src/main/java/com/zhxh/codeproj/multithread/KeywordStop.java
@@ -0,0 +1,38 @@
+package com.zhxh.codeproj.multithread;
+
+public class KeywordStop {
+    public static void main(String[] args) throws InterruptedException {
+        KeywordStopThread thread = new KeywordStopThread("a");
+        new Thread(thread).start();
+        Thread.sleep(3000);
+        thread.isExit = true;
+    }
+}
+
+class KeywordStopThread implements Runnable {
+    private String name;
+
+    public KeywordStopThread(String name) {
+        this.name = name;
+    }
+
+    /*
+    退出线程的标志
+    volatile是因为可以保证有序性和可见性
+    这里需要修改 isExit 以后里面对主内存可见。
+     */
+    public volatile boolean isExit = false;
+
+    @Override
+    public void run() {
+        while (!isExit) {
+            try {
+                Thread.sleep(1000);
+                System.out.println("线程 " + name + " 正在运行中");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("线程终止");
+    }
+}
