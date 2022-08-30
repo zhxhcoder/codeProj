1eccfa91f211dbe983fe8b5d1b8a1d9800414ae6
zhxh <zhxhcoder@gmail.com>
2022-08-30 16:53:50 +0800

    Graph图DFS深度优先搜索

--- app/src/main/java/com/zhxh/codeproj/leetcode/LeetCode787.java
+++ app/src/main/java/com/zhxh/codeproj/leetcode/LeetCode787.java
@@ -1,7 +1,136 @@
 package com.zhxh.codeproj.leetcode;
 
+import java.util.Arrays;
+
+/*
+有 n 个城市通过一些航班连接。给你一个数组flights ，其中flights[i] = [fromi, toi, pricei] ，表示该航班都从城市 fromi 开始，以价格 pricei 抵达 toi。
+现在给定所有的城市和航班，以及出发城市 src 和目的地 dst，你的任务是找到出一条最多经过 k站中转的路线，使得从 src 到 dst 的 价格最便宜 ，并返回该价格。 如果不存在这样的路线，则输出 -1。
+
+示例 1：
+
+输入: 
+n = 3, edges = [[0,1,100],[1,2,100],[0,2,500]]
+src = 0, dst = 2, k = 1
+输出: 200
+解释: 
+城市航班图如下
+
+
+从城市 0 到城市 2 在 1 站中转以内的最便宜价格是 200，如图中红色所示。
+示例 2：
+
+输入: 
+n = 3, edges = [[0,1,100],[1,2,100],[0,2,500]]
+src = 0, dst = 2, k = 0
+输出: 500
+解释: 
+城市航班图如下
+
+
+从城市 0 到城市 2 在 0 站中转以内的最便宜价格是 500，如图中蓝色所示。
+
+
+提示：
+
+1 <= n <= 100
+0 <= flights.length <= (n * (n - 1) / 2)
+flights[i].length == 3
+0 <= fromi, toi < n
+fromi != toi
+1 <= pricei <= 104
+航班没有重复，且不存在自环
+0 <= src, dst, k < n
+src != dst
+
+ */
 public class LeetCode787 {
     public static void main(String[] args) {
+        System.out.println(new Solution().findCheapestPrice(3, new int[][]{{0, 1, 100}, {1, 2, 100}, {0, 2, 500}}, 0, 2, 0));
+        System.out.println(new Solution2().findCheapestPrice(3, new int[][]{{0, 1, 100}, {1, 2, 100}, {0, 2, 500}}, 0, 2, 0));
+        System.out.println(new Solution3().findCheapestPrice(3, new int[][]{{0, 1, 100}, {1, 2, 100}, {0, 2, 500}}, 0, 2, 0));
+    }
 
+    /*
+    动态数组
+     */
+    static class Solution {
+        public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+            final int INF = 10000 * 101 + 1;
+            int[][] f = new int[k + 2][n];
+            for (int i = 0; i < k + 2; ++i) {
+                Arrays.fill(f[i], INF);
+            }
+            f[0][src] = 0;
+            for (int t = 1; t <= k + 1; ++t) {
+                for (int[] flight : flights) {
+                    int j = flight[0], i = flight[1], cost = flight[2];
+                    f[t][i] = Math.min(f[t][i], f[t - 1][j] + cost);
+                }
+            }
+            int ans = INF;
+            for (int t = 1; t <= k + 1; ++t) {
+                ans = Math.min(ans, f[t][dst]);
+            }
+            return ans == INF ? -1 : ans;
+        }
+    }
+
+    /*
+    下面的代码使用两个一维数组进行状态转移。
+     */
+    static class Solution2 {
+        public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+            final int INF = 10000 * 101 + 1;
+            int[] f = new int[n];
+            Arrays.fill(f, INF);
+            f[src] = 0;
+            int ans = INF;
+            for (int t = 1; t <= k + 1; ++t) {
+                int[] g = new int[n];
+                Arrays.fill(g, INF);
+                for (int[] flight : flights) {
+                    int j = flight[0], i = flight[1], cost = flight[2];
+                    g[i] = Math.min(g[i], f[j] + cost);
+                }
+                f = g;
+                ans = Math.min(ans, f[dst]);
+            }
+            return ans == INF ? -1 : ans;
+        }
+    }
+
+    /*
+    自己尝试
+     */
+    static class Solution3 {
+        public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+            final int INF = 10000 * 101 + 1;
+            //f[t][i] 表示从src到 i 中转t次的最少价格
+            int[][] f = new int[k + 1][n];
+            //初始化为最大值
+            for (int i = 0; i < k + 1; ++i) {
+                Arrays.fill(f[i], INF);
+            }
+            //单独处理k == 0 的情况，就是不需要中转
+            for (int[] flight : flights) {
+                int j = flight[0], i = flight[1], cost = flight[2];
+                if (j == src) {
+                    f[0][i] = cost;
+                }
+            }
+            for (int t = 1; t < k + 1; ++t) {
+                for (int[] flight : flights) {
+                    int j = flight[0], i = flight[1], cost = flight[2];
+                    //从后往前推，从j中转第t次到达i的最少价格
+                    f[t][i] = Math.min(f[t][i], f[t - 1][j] + cost);
+                }
+            }
+            int ans = INF;
+            //遍历最多经过 k 站中转的路线，获取最少价格
+            for (int t = 0; t <= k; ++t) {
+                ans = Math.min(ans, f[t][dst]);
+            }
+            return ans == INF ? -1 : ans;
+        }
     }
 }
