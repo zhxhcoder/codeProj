ecb8a989ddb9dc26613a1b833723493c3bdab331
zhxh <zhxhcoder@gmail.com>
2022-08-31 19:39:02 +0800

    贪心算法|动态规划

--- app/src/main/java/com/zhxh/codeproj/leetcode/top145/LeetCode118.java
+++ app/src/main/java/com/zhxh/codeproj/leetcode/top145/LeetCode118.java
@@ -1,7 +1,55 @@
 package com.zhxh.codeproj.leetcode.top145;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+给定一个非负整数 numRows，生成「杨辉三角」的前 numRows 行。
+
+在「杨辉三角」中，每个数是它左上方和右上方的数的和。
+
+
+
+ 
+
+示例 1:
+
+输入: numRows = 5
+输出: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+示例 2:
+
+输入: numRows = 1
+输出: [[1]]
+ 
+
+提示:
+
+1 <= numRows <= 30
+
+ */
 public class LeetCode118 {
     public static void main(String[] args) {
+        System.out.println(new Solution().generate(5));
+    }
 
+    /*
+    方法一：数学
+     */
+    static class Solution {
+        public List<List<Integer>> generate(int numRows) {
+            List<List<Integer>> ret = new ArrayList<List<Integer>>();
+            for (int i = 0; i < numRows; ++i) {
+                List<Integer> row = new ArrayList<Integer>();
+                for (int j = 0; j <= i; ++j) {
+                    if (j == 0 || j == i) {
+                        row.add(1);
+                    } else {
+                        row.add(ret.get(i - 1).get(j - 1) + ret.get(i - 1).get(j));
+                    }
+                }
+                ret.add(row);
+            }
+            return ret;
+        }
     }
 }
