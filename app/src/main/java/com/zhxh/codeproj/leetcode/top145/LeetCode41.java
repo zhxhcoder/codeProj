38128e4b78318e5dbb712ee14a38b7eff61f6e17
zhxh <zhxhcoder@gmail.com>
2022-08-31 15:11:52 +0800

    罗马数组转整数

--- app/src/main/java/com/zhxh/codeproj/leetcode/top145/LeetCode41.java
+++ app/src/main/java/com/zhxh/codeproj/leetcode/top145/LeetCode41.java
@@ -1,7 +1,91 @@
 package com.zhxh.codeproj.leetcode.top145;
 
+/*
+给你一个未排序的整数数组 nums ，请你找出其中没有出现的最小的正整数。
+
+请你实现时间复杂度为 O(n) 并且只使用常数级别额外空间的解决方案。
+ 
+
+示例 1：
+
+输入：nums = [1,2,0]
+输出：3
+示例 2：
+
+输入：nums = [3,4,-1,1]
+输出：2
+示例 3：
+
+输入：nums = [7,8,9,11,12]
+输出：1
+ 
+
+提示：
+
+1 <= nums.length <= 5 * 105
+-231 <= nums[i] <= 231 - 1
+
+ */
 public class LeetCode41 {
     public static void main(String[] args) {
+        System.out.println(new Solution().firstMissingPositive(new int[]{3, 4, -1, 1}));
+        System.out.println(new Solution2().firstMissingPositive(new int[]{3, 4, -1, 1}));
+    }
 
+    /*
+    方法一：哈希表
+    我们可以将数组所有的数放入哈希表中，随后从1开始依次枚举正整数，并判断是否在哈希表中。
+    将数组设计成哈希表中的思路：
+    我们对数组进行遍历，对于遍历到的数 x，如果它在 [1,N] 的范围内，那么就将数组中的第 x−1 个位置（注意：数组下标从 0 开始）打上「标记」。
+    在遍历结束之后，如果所有的位置都被打上了标记，那么答案是 N+1，否则答案是最小的没有打上标记的位置加 1。
+
+     */
+    static class Solution {
+        public int firstMissingPositive(int[] nums) {
+            int n = nums.length;
+            for (int i = 0; i < n; ++i) {
+                if (nums[i] <= 0) {
+                    nums[i] = n + 1;
+                }
+            }
+            for (int i = 0; i < n; ++i) {
+                int num = Math.abs(nums[i]);
+                if (num <= n) {
+                    nums[num - 1] = -Math.abs(nums[num - 1]);
+                }
+            }
+            for (int i = 0; i < n; ++i) {
+                if (nums[i] > 0) {
+                    return i + 1;
+                }
+            }
+            return n + 1;
+        }
+    }
+
+    /*
+    方法二：置换
+     */
+    static class Solution2 {
+        public int firstMissingPositive(int[] nums) {
+            int n = nums.length;
+            for (int i = 0; i < n; ++i) {
+                if (nums[i] <= 0) {
+                    nums[i] = n + 1;
+                }
+            }
+            for (int i = 0; i < n; ++i) {
+                int num = Math.abs(nums[i]);
+                if (num <= n) {
+                    nums[num - 1] = -Math.abs(nums[num - 1]);
+                }
+            }
+            for (int i = 0; i < n; ++i) {
+                if (nums[i] > 0) {
+                    return i + 1;
+                }
+            }
+            return n + 1;
+        }
     }
 }
