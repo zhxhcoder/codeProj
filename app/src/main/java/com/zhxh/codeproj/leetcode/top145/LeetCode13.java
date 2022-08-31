7e612501352cd44a3878e525adc6b9ffce22d56d
zhxh <zhxhcoder@gmail.com>
2022-08-31 14:14:42 +0800

    罗马数组转整数

--- app/src/main/java/com/zhxh/codeproj/leetcode/top145/LeetCode13.java
+++ app/src/main/java/com/zhxh/codeproj/leetcode/top145/LeetCode13.java
@@ -66,6 +66,11 @@ public class LeetCode13 {
 
     /*
     模拟
+    通常情况下，罗马数字中小的数字在大的数字的右边。若输入的字符串满足该情况，那么可以将每个字符视作一个单独的值，
+    累加每个字符字符对应的数值即可。
+
+    若存在小的数字在大的数字的左边的情况，根据规则需要减去小的数字。对于这种情况，我们也可以将每个字符视作一个单独的值，
+    若一个数字右侧的数字比它大，则将该数字的符号取反
      */
     static class Solution {
         Map<Character, Integer> symbolValues = new HashMap<Character, Integer>() {{
@@ -83,6 +88,7 @@ public class LeetCode13 {
             int n = s.length();
             for (int i = 0; i < n; ++i) {
                 int value = symbolValues.get(s.charAt(i));
+                //i < n - 1 防止越界；小于右侧时，符号取反
                 if (i < n - 1 && value < symbolValues.get(s.charAt(i + 1))) {
                     ans -= value;
                 } else {
