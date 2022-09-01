5a1b48498759159471e16871b367dc6c503f99ac
zhxh <zhxhcoder@gmail.com>
2022-09-01 08:54:58 +0800

    广度优先遍历建模

--- app/src/main/java/com/zhxh/codeproj/leetcode/top145/LeetCode127.java
+++ app/src/main/java/com/zhxh/codeproj/leetcode/top145/LeetCode127.java
@@ -1,7 +1,199 @@
 package com.zhxh.codeproj.leetcode.top145;
 
+import java.util.*;
+
+/*
+字典 wordList 中从单词 beginWord 和 endWord 的 转换序列 是一个按下述规格形成的序列 beginWord -> s1 -> s2 -> ... -> sk：
+
+每一对相邻的单词只差一个字母。
+ 对于 1 <= i <= k 时，每个 si 都在 wordList 中。注意， beginWord 不需要在 wordList 中。
+sk == endWord
+给你两个单词 beginWord 和 endWord 和一个字典 wordList ，返回 从 beginWord 到 endWord 的 最短转换序列 中的 单词数目 。如果不存在这样的转换序列，返回 0 。
+
+ 
+示例 1：
+
+输入：beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log","cog"]
+输出：5
+解释：一个最短转换序列是 "hit" -> "hot" -> "dot" -> "dog" -> "cog", 返回它的长度 5。
+示例 2：
+
+输入：beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log"]
+输出：0
+解释：endWord "cog" 不在字典中，所以无法进行转换。
+ 
+
+提示：
+
+1 <= beginWord.length <= 10
+endWord.length == beginWord.length
+1 <= wordList.length <= 5000
+wordList[i].length == beginWord.length
+beginWord、endWord 和 wordList[i] 由小写英文字母组成
+beginWord != endWord
+wordList 中的所有字符串 互不相同
+
+ */
 public class LeetCode127 {
     public static void main(String[] args) {
+        //todo bfs广度优先
+        System.out.println(new Solution().ladderLength("hit", "cog", Arrays.asList("hot", "dot", "dog", "lot", "log", "cog")));
+        System.out.println(new Solution2().ladderLength("hit", "cog", Arrays.asList("hot", "dot", "dog", "lot", "log", "cog")));
+    }
 
+    /*
+    方法一：广度优先搜索 + 优化建图
+    本题要求的是最短转换序列的长度，看到最短首先想到的就是广度优先搜索。
+    想到广度优先搜索自然而然的就能想到图，但是本题并没有直截了当的给出图的模型，因此我们需要把它抽象成图的模型。
+     */
+    static class Solution {
+        Map<String, Integer> wordId = new HashMap<String, Integer>();
+        List<List<Integer>> edge = new ArrayList<List<Integer>>();
+        int nodeNum = 0;
+
+        public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+            for (String word : wordList) {
+                addEdge(word);
+            }
+            addEdge(beginWord);
+            if (!wordId.containsKey(endWord)) {
+                return 0;
+            }
+            int[] dis = new int[nodeNum];
+            Arrays.fill(dis, Integer.MAX_VALUE);
+            int beginId = wordId.get(beginWord), endId = wordId.get(endWord);
+            dis[beginId] = 0;
+
+            Queue<Integer> que = new LinkedList<Integer>();
+            que.offer(beginId);
+            while (!que.isEmpty()) {
+                int x = que.poll();
+                if (x == endId) {
+                    return dis[endId] / 2 + 1;
+                }
+                for (int it : edge.get(x)) {
+                    if (dis[it] == Integer.MAX_VALUE) {
+                        dis[it] = dis[x] + 1;
+                        que.offer(it);
+                    }
+                }
+            }
+            return 0;
+        }
+
+        public void addEdge(String word) {
+            addWord(word);
+            int id1 = wordId.get(word);
+            char[] array = word.toCharArray();
+            int length = array.length;
+            for (int i = 0; i < length; ++i) {
+                char tmp = array[i];
+                array[i] = '*';
+                String newWord = new String(array);
+                addWord(newWord);
+                int id2 = wordId.get(newWord);
+                edge.get(id1).add(id2);
+                edge.get(id2).add(id1);
+                array[i] = tmp;
+            }
+        }
+
+        public void addWord(String word) {
+            if (!wordId.containsKey(word)) {
+                wordId.put(word, nodeNum++);
+                edge.add(new ArrayList<Integer>());
+            }
+        }
+    }
+
+    /*
+    方法二：双向广度优先搜索
+    根据给定字典构造的图可能会很大，而广度优先搜索的搜索空间大小依赖于每层节点的分支数量。
+    假如每个节点的分支数量相同，搜索空间会随着层数的增长指数级的增加。
+    考虑一个简单的二叉树，每一层都是满二叉树的扩展，节点的数量会以 2 为底数呈指数增长。
+     */
+    static class Solution2 {
+        Map<String, Integer> wordId = new HashMap<String, Integer>();
+        List<List<Integer>> edge = new ArrayList<List<Integer>>();
+        int nodeNum = 0;
+
+        public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+            for (String word : wordList) {
+                addEdge(word);
+            }
+            addEdge(beginWord);
+            if (!wordId.containsKey(endWord)) {
+                return 0;
+            }
+
+            int[] disBegin = new int[nodeNum];
+            Arrays.fill(disBegin, Integer.MAX_VALUE);
+            int beginId = wordId.get(beginWord);
+            disBegin[beginId] = 0;
+            Queue<Integer> queBegin = new LinkedList<Integer>();
+            queBegin.offer(beginId);
+
+            int[] disEnd = new int[nodeNum];
+            Arrays.fill(disEnd, Integer.MAX_VALUE);
+            int endId = wordId.get(endWord);
+            disEnd[endId] = 0;
+            Queue<Integer> queEnd = new LinkedList<Integer>();
+            queEnd.offer(endId);
+
+            while (!queBegin.isEmpty() && !queEnd.isEmpty()) {
+                int queBeginSize = queBegin.size();
+                for (int i = 0; i < queBeginSize; ++i) {
+                    int nodeBegin = queBegin.poll();
+                    if (disEnd[nodeBegin] != Integer.MAX_VALUE) {
+                        return (disBegin[nodeBegin] + disEnd[nodeBegin]) / 2 + 1;
+                    }
+                    for (int it : edge.get(nodeBegin)) {
+                        if (disBegin[it] == Integer.MAX_VALUE) {
+                            disBegin[it] = disBegin[nodeBegin] + 1;
+                            queBegin.offer(it);
+                        }
+                    }
+                }
+
+                int queEndSize = queEnd.size();
+                for (int i = 0; i < queEndSize; ++i) {
+                    int nodeEnd = queEnd.poll();
+                    if (disBegin[nodeEnd] != Integer.MAX_VALUE) {
+                        return (disBegin[nodeEnd] + disEnd[nodeEnd]) / 2 + 1;
+                    }
+                    for (int it : edge.get(nodeEnd)) {
+                        if (disEnd[it] == Integer.MAX_VALUE) {
+                            disEnd[it] = disEnd[nodeEnd] + 1;
+                            queEnd.offer(it);
+                        }
+                    }
+                }
+            }
+            return 0;
+        }
+
+        public void addEdge(String word) {
+            addWord(word);
+            int id1 = wordId.get(word);
+            char[] array = word.toCharArray();
+            int length = array.length;
+            for (int i = 0; i < length; ++i) {
+                char tmp = array[i];
+                array[i] = '*';
+                String newWord = new String(array);
+                addWord(newWord);
+                int id2 = wordId.get(newWord);
+                edge.get(id1).add(id2);
+                edge.get(id2).add(id1);
+                array[i] = tmp;
+            }
+        }
+
+        public void addWord(String word) {
+            if (!wordId.containsKey(word)) {
+                wordId.put(word, nodeNum++);
+                edge.add(new ArrayList<Integer>());
+            }
+        }
     }
 }
