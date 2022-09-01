2afdff8b90dbd14e058d0d4753eb3b5e5eb7f1c5
zhxh <zhxhcoder@gmail.com>
2022-09-01 16:15:38 +0800

    长除法

--- app/src/main/java/com/zhxh/codeproj/leetcode/top145/LeetCode172.java
+++ app/src/main/java/com/zhxh/codeproj/leetcode/top145/LeetCode172.java
@@ -1,7 +1,68 @@
 package com.zhxh.codeproj.leetcode.top145;
 
+/*
+给定一个整数 n ，返回 n! 结果中尾随零的数量。
+
+提示 n! = n * (n - 1) * (n - 2) * ... * 3 * 2 * 1
+
+ 
+
+示例 1：
+
+输入：n = 3
+输出：0
+解释：3! = 6 ，不含尾随 0
+示例 2：
+
+输入：n = 5
+输出：1
+解释：5! = 120 ，有一个尾随 0
+示例 3：
+
+输入：n = 0
+输出：0
+ 
+
+提示：
+
+0 <= n <= 104
+ 
+
+进阶：你可以设计并实现对数时间复杂度的算法来解决此问题吗？
+
+ */
 public class LeetCode172 {
     public static void main(String[] args) {
+        System.out.println(new Solution().trailingZeroes(13));
+        System.out.println(new Solution2().trailingZeroes(13));
+    }
 
+    /*
+    方法一：数学
+     */
+    static class Solution {
+        public int trailingZeroes(int n) {
+            int ans = 0;
+            for (int i = 5; i <= n; i += 5) {
+                for (int x = i; x % 5 == 0; x /= 5) {
+                    ++ans;
+                }
+            }
+            return ans;
+        }
+    }
+
+    /*
+    方法二：优化计算
+     */
+    static class Solution2 {
+        public int trailingZeroes(int n) {
+            int ans = 0;
+            while (n != 0) {
+                n /= 5;
+                ans += n;
+            }
+            return ans;
+        }
     }
 }
--- app/src/main/java/com/zhxh/codeproj/leetcode/top145/LeetCode179.java
+++ app/src/main/java/com/zhxh/codeproj/leetcode/top145/LeetCode179.java
@@ -1,7 +1,100 @@
 package com.zhxh.codeproj.leetcode.top145;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
+/*
+给定一组非负整数 nums，重新排列每个数的顺序（每个数不可拆分）使之组成一个最大的整数。
+
+注意：输出结果可能非常大，所以你需要返回一个字符串而不是整数。
+
+ 
+
+示例 1：
+
+输入：nums = [10,2]
+输出："210"
+示例 2：
+
+输入：nums = [3,30,34,5,9]
+输出："9534330"
+ 
+
+提示：
+
+1 <= nums.length <= 100
+0 <= nums[i] <= 109
+
+ */
 public class LeetCode179 {
     public static void main(String[] args) {
+        System.out.println(new Solution().largestNumber(new int[]{3, 30, 34, 5, 9}));
+        System.out.println(new Solution2().largestNumber(new int[]{3, 30, 34, 5, 9}));
+    }
 
+    /*
+    方法一：排序
+     */
+    static class Solution {
+        public String largestNumber(int[] nums) {
+            int n = nums.length;
+            // 转换成包装类型，以便传入 Comparator 对象（此处为 lambda 表达式）
+            Integer[] numsArr = new Integer[n];
+            for (int i = 0; i < n; i++) {
+                numsArr[i] = nums[i];
+            }
+
+            Arrays.sort(numsArr, (x, y) -> {
+                long sx = 10, sy = 10;
+                while (sx <= x) {
+                    sx *= 10;
+                }
+                while (sy <= y) {
+                    sy *= 10;
+                }
+                return (int) (-sy * x - y + sx * y + x);
+            });
+
+            if (numsArr[0] == 0) {
+                return "0";
+            }
+            StringBuilder ret = new StringBuilder();
+            for (int num : numsArr) {
+                ret.append(num);
+            }
+            return ret.toString();
+        }
+    }
+
+    static class Solution2 {
+        private class LargerNumberComparator implements Comparator<String> {
+            @Override
+            public int compare(String a, String b) {
+                String order1 = a + b;
+                String order2 = b + a;
+                return order2.compareTo(order1);
+            }
+        }
+
+        public String largestNumber(int[] nums) {
+            // 获取输入整数作为字符串。
+            String[] asStrs = new String[nums.length];
+            for (int i = 0; i < nums.length; i++) {
+                asStrs[i] = String.valueOf(nums[i]);
+            }
+            // 根据自定义比较器对字符串进行排序。
+            Arrays.sort(asStrs, new LargerNumberComparator());
+
+            // 如果排序后最大的数为0，则整个数为零。
+            if (asStrs[0].equals("0")) {
+                return "0";
+            }
+            // 从排序数组中构建最大数。
+            String largestNumberStr = new String();
+            for (String numAsStr : asStrs) {
+                largestNumberStr += numAsStr;
+            }
+            return largestNumberStr;
+        }
     }
 }
