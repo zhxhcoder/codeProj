80942ba5d4c5943ec555f915b84666f0b34f952d
zhxh <zhxhcoder@gmail.com>
2022-09-02 15:34:47 +0800

    分治算法

--- app/src/main/java/com/zhxh/codeproj/leetcode/top145/LeetCode387.java
+++ app/src/main/java/com/zhxh/codeproj/leetcode/top145/LeetCode387.java
@@ -1,7 +1,121 @@
 package com.zhxh.codeproj.leetcode.top145;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+
+/*
+387. 字符串中的第一个唯一字符
+给定一个字符串 s ，找到 它的第一个不重复的字符，并返回它的索引 。如果不存在，则返回 -1 。
+
+
+示例 1：
+
+输入: s = "leetcode"
+输出: 0
+示例 2:
+
+输入: s = "loveleetcode"
+输出: 2
+示例 3:
+
+输入: s = "aabb"
+输出: -1
+
+
+提示:
+
+1 <= s.length <= 105
+s 只包含小写字母
+ */
 public class LeetCode387 {
     public static void main(String[] args) {
+        System.out.println(new Solution().firstUniqChar("loveleetcode"));
+        System.out.println(new Solution2().firstUniqChar("loveleetcode"));
+        System.out.println(new Solution3().firstUniqChar("loveleetcode"));
+    }
 
+    /*
+    方法一：使用哈希表存储频数
+     */
+    static class Solution {
+        public int firstUniqChar(String s) {
+            Map<Character, Integer> frequency = new HashMap<Character, Integer>();
+            for (int i = 0; i < s.length(); ++i) {
+                char ch = s.charAt(i);
+                frequency.put(ch, frequency.getOrDefault(ch, 0) + 1);
+            }
+            for (int i = 0; i < s.length(); ++i) {
+                if (frequency.get(s.charAt(i)) == 1) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+
+
+    /*
+    方法二：使用哈希表存储索引
+     */
+    static class Solution2 {
+        public int firstUniqChar(String s) {
+            Map<Character, Integer> position = new HashMap<Character, Integer>();
+            int n = s.length();
+            for (int i = 0; i < n; ++i) {
+                char ch = s.charAt(i);
+                if (position.containsKey(ch)) {
+                    position.put(ch, -1);
+                } else {
+                    position.put(ch, i);
+                }
+            }
+            int first = n;
+            for (Map.Entry<Character, Integer> entry : position.entrySet()) {
+                int pos = entry.getValue();
+                if (pos != -1 && pos < first) {
+                    first = pos;
+                }
+            }
+            if (first == n) {
+                first = -1;
+            }
+            return first;
+        }
+    }
+
+    /*
+    方法三：队列
+     */
+    static class Solution3 {
+        public int firstUniqChar(String s) {
+            Map<Character, Integer> position = new HashMap<Character, Integer>();
+            Queue<Pair> queue = new LinkedList<Pair>();
+            int n = s.length();
+            for (int i = 0; i < n; ++i) {
+                char ch = s.charAt(i);
+                if (!position.containsKey(ch)) {
+                    position.put(ch, i);
+                    queue.offer(new Pair(ch, i));
+                } else {
+                    position.put(ch, -1);
+                    while (!queue.isEmpty() && position.get(queue.peek().ch) == -1) {
+                        queue.poll();
+                    }
+                }
+            }
+            return queue.isEmpty() ? -1 : queue.poll().pos;
+        }
+
+        static class Pair {
+            char ch;
+            int pos;
+
+            Pair(char ch, int pos) {
+                this.ch = ch;
+                this.pos = pos;
+            }
+        }
     }
 }
