1c980dfc87e06b2175768c6f749e0e76ad7fd5f0
zhxh <zhxhcoder@gmail.com>
2022-09-02 15:41:22 +0800

    分治算法

--- app/src/main/java/com/zhxh/codeproj/leetcode/top145/LeetCode412.java
+++ app/src/main/java/com/zhxh/codeproj/leetcode/top145/LeetCode412.java
@@ -1,7 +1,61 @@
 package com.zhxh.codeproj.leetcode.top145;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+412. Fizz Buzz
+给你一个整数 n ，找出从 1 到 n 各个整数的 Fizz Buzz 表示，并用字符串数组 answer（下标从 1 开始）返回结果，其中：
+
+answer[i] == "FizzBuzz" 如果 i 同时是 3 和 5 的倍数。
+answer[i] == "Fizz" 如果 i 是 3 的倍数。
+answer[i] == "Buzz" 如果 i 是 5 的倍数。
+answer[i] == i （以字符串形式）如果上述条件全不满足。
+
+
+示例 1：
+
+输入：n = 3
+输出：["1","2","Fizz"]
+示例 2：
+
+输入：n = 5
+输出：["1","2","Fizz","4","Buzz"]
+示例 3：
+
+输入：n = 15
+输出：["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
+
+
+提示：
+
+1 <= n <= 104
+ */
 public class LeetCode412 {
     public static void main(String[] args) {
-        
+        System.out.println(new Solution().fizzBuzz(5));
+    }
+
+    /*
+    方法一：模拟 + 字符串拼接
+     */
+    static class Solution {
+        public List<String> fizzBuzz(int n) {
+            List<String> answer = new ArrayList<String>();
+            for (int i = 1; i <= n; i++) {
+                StringBuffer sb = new StringBuffer();
+                if (i % 3 == 0) {
+                    sb.append("Fizz");
+                }
+                if (i % 5 == 0) {
+                    sb.append("Buzz");
+                }
+                if (sb.length() == 0) {
+                    sb.append(i);
+                }
+                answer.add(sb.toString());
+            }
+            return answer;
+        }
     }
 }
--- app/src/main/java/com/zhxh/codeproj/leetcode/top145/LeetCode454.java
+++ app/src/main/java/com/zhxh/codeproj/leetcode/top145/LeetCode454.java
@@ -1,7 +1,66 @@
 package com.zhxh.codeproj.leetcode.top145;
 
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+454. 四数相加 II
+给你四个整数数组 nums1、nums2、nums3 和 nums4 ，数组长度都是 n ，请你计算有多少个元组 (i, j, k, l) 能满足：
+
+0 <= i, j, k, l < n
+nums1[i] + nums2[j] + nums3[k] + nums4[l] == 0
+
+
+示例 1：
+
+输入：nums1 = [1,2], nums2 = [-2,-1], nums3 = [-1,2], nums4 = [0,2]
+输出：2
+解释：
+两个元组如下：
+1. (0, 0, 0, 1) -> nums1[0] + nums2[0] + nums3[0] + nums4[1] = 1 + (-2) + (-1) + 2 = 0
+2. (1, 1, 0, 0) -> nums1[1] + nums2[1] + nums3[0] + nums4[0] = 2 + (-1) + (-1) + 0 = 0
+示例 2：
+
+输入：nums1 = [0], nums2 = [0], nums3 = [0], nums4 = [0]
+输出：1
+
+
+  提示：
+
+n == nums1.length
+n == nums2.length
+n == nums3.length
+n == nums4.length
+1 <= n <= 200
+-228 <= nums1[i], nums2[i], nums3[i], nums4[i] <= 228
+
+ */
 public class LeetCode454 {
     public static void main(String[] args) {
+        System.out.println(new Solution().fourSumCount(new int[]{1, 2}, new int[]{-2, -1}, new int[]{-1, 2}, new int[]{0, 2}));
+    }
 
+    /*
+    方法一：分组 + 哈希表
+    我们可以将四个数组分成两部分，AA 和 BB 为一组，CC 和 DD 为另外一组。
+     */
+    static class Solution {
+        public int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
+            Map<Integer, Integer> countAB = new HashMap<Integer, Integer>();
+            for (int u : A) {
+                for (int v : B) {
+                    countAB.put(u + v, countAB.getOrDefault(u + v, 0) + 1);
+                }
+            }
+            int ans = 0;
+            for (int u : C) {
+                for (int v : D) {
+                    if (countAB.containsKey(-u - v)) {
+                        ans += countAB.get(-u - v);
+                    }
+                }
+            }
+            return ans;
+        }
     }
 }
