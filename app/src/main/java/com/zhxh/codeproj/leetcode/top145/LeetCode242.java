8188bdcc287a3c3698221fad34ca66afd5f7467b
zhxh <zhxhcoder@gmail.com>
2022-09-02 09:26:44 +0800

    哈希表

--- app/src/main/java/com/zhxh/codeproj/leetcode/top145/LeetCode242.java
+++ app/src/main/java/com/zhxh/codeproj/leetcode/top145/LeetCode242.java
@@ -1,7 +1,102 @@
 package com.zhxh.codeproj.leetcode.top145;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
+
+注意：若 s 和 t 中每个字符出现的次数都相同，则称 s 和 t 互为字母异位词。
+
+ 
+
+示例 1:
+
+输入: s = "anagram", t = "nagaram"
+输出: true
+示例 2:
+
+输入: s = "rat", t = "car"
+输出: false
+ 
+
+提示:
+
+1 <= s.length, t.length <= 5 * 104
+s 和 t 仅包含小写字母
+ 
+
+进阶: 如果输入字符串包含 unicode 字符怎么办？你能否调整你的解法来应对这种情况？
+
+ */
 public class LeetCode242 {
     public static void main(String[] args) {
+        System.out.println(new Solution().isAnagram("anagram", "nagaram"));
+        System.out.println(new Solution2().isAnagram("anagram", "nagaram"));
+        System.out.println(new Solution3().isAnagram("anagram", "nagaram"));
+    }
 
+    /*
+    方法一：排序
+     */
+    static class Solution {
+        public boolean isAnagram(String s, String t) {
+            if (s.length() != t.length()) {
+                return false;
+            }
+            char[] str1 = s.toCharArray();
+            char[] str2 = t.toCharArray();
+            Arrays.sort(str1);
+            Arrays.sort(str2);
+            return Arrays.equals(str1, str2);
+        }
+    }
+
+    /*
+    方法二：哈希表
+     */
+    static class Solution2 {
+        public boolean isAnagram(String s, String t) {
+            if (s.length() != t.length()) {
+                return false;
+            }
+            int[] table = new int[26];
+            for (int i = 0; i < s.length(); i++) {
+                table[s.charAt(i) - 'a']++;
+            }
+            for (int i = 0; i < t.length(); i++) {
+                table[t.charAt(i) - 'a']--;
+                if (table[t.charAt(i) - 'a'] < 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+    /*
+    对于进阶问题，Unicode 是为了解决传统字符编码的局限性而产生的方案，
+    它为每个语言中的字符规定了一个唯一的二进制编码
+     */
+    static class Solution3 {
+        public boolean isAnagram(String s, String t) {
+            if (s.length() != t.length()) {
+                return false;
+            }
+            Map<Character, Integer> table = new HashMap<Character, Integer>();
+            for (int i = 0; i < s.length(); i++) {
+                char ch = s.charAt(i);
+                table.put(ch, table.getOrDefault(ch, 0) + 1);
+            }
+            for (int i = 0; i < t.length(); i++) {
+                char ch = t.charAt(i);
+                table.put(ch, table.getOrDefault(ch, 0) - 1);
+                if (table.get(ch) < 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
     }
 }
