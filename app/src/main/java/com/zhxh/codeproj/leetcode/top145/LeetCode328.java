b9d1292211d56fb57f1f256d9e54bf3d435d8f25
zhxh <zhxhcoder@gmail.com>
2022-09-02 13:27:14 +0800

    深度优先排序

--- app/src/main/java/com/zhxh/codeproj/leetcode/top145/LeetCode328.java
+++ app/src/main/java/com/zhxh/codeproj/leetcode/top145/LeetCode328.java
@@ -1,7 +1,69 @@
 package com.zhxh.codeproj.leetcode.top145;
 
+import com.zhxh.codeproj.leetcode.__base.ListNode;
+
+/*
+给定单链表的头节点 head ，将所有索引为奇数的节点和索引为偶数的节点分别组合在一起，然后返回重新排序的列表。
+
+第一个节点的索引被认为是 奇数 ， 第二个节点的索引为 偶数 ，以此类推。
+
+请注意，偶数组和奇数组内部的相对顺序应该与输入时保持一致。
+
+你必须在 O(1) 的额外空间复杂度和 O(n) 的时间复杂度下解决这个问题。
+
+ 
+
+示例 1:
+
+
+
+输入: head = [1,2,3,4,5]
+输出: [1,3,5,2,4]
+示例 2:
+
+
+
+输入: head = [2,1,3,5,6,4,7]
+输出: [2,3,6,7,1,5,4]
+ 
+
+提示:
+
+n ==  链表中的节点数
+0 <= n <= 104
+-106 <= Node.val <= 106
+
+ */
 public class LeetCode328 {
     public static void main(String[] args) {
+        ListNode.printNode(new Solution().oddEvenList(ListNode.buildNode(new int[]{1, 2, 3, 4, 5})));
+    }
 
+    /*
+    方法一：分离节点后合并
+     */
+    static class Solution {
+        public ListNode oddEvenList(ListNode head) {
+            if (head == null) {
+                return head;
+            }
+            //偶数头节点，头部
+            ListNode evenHead = head.next;
+            //奇数头节点
+            ListNode odd = head;
+            //偶数链表
+            ListNode even = evenHead;
+            while (even != null && even.next != null) {
+                //删除偶数节点
+                odd.next = even.next;
+                odd = odd.next;
+                //连接偶数节点
+                even.next = odd.next;
+                even = even.next;
+            }
+            //拼接
+            odd.next = evenHead;
+            return head;
+        }
     }
 }
--- app/src/main/java/com/zhxh/codeproj/leetcode/top145/LeetCode329.java
+++ app/src/main/java/com/zhxh/codeproj/leetcode/top145/LeetCode329.java
@@ -1,7 +1,136 @@
 package com.zhxh.codeproj.leetcode.top145;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
+/*
+给定一个 m x n 整数矩阵 matrix ，找出其中 最长递增路径 的长度。
+
+对于每个单元格，你可以往上，下，左，右四个方向移动。 你 不能 在 对角线 方向上移动或移动到 边界外（即不允许环绕）。
+
+ 
+
+示例 1：
+
+
+输入：matrix = [[9,9,4],[6,6,8],[2,1,1]]
+输出：4
+解释：最长递增路径为 [1, 2, 6, 9]。
+示例 2：
+
+
+输入：matrix = [[3,4,5],[3,2,6],[2,2,1]]
+输出：4
+解释：最长递增路径是 [3, 4, 5, 6]。注意不允许在对角线方向上移动。
+示例 3：
+
+输入：matrix = [[1]]
+输出：1
+ 
+
+提示：
+
+m == matrix.length
+n == matrix[i].length
+1 <= m, n <= 200
+0 <= matrix[i][j] <= 231 - 1
+
+ */
 public class LeetCode329 {
     public static void main(String[] args) {
+        System.out.println(new Solution().longestIncreasingPath(new int[][]{{9, 9, 4}, {6, 6, 8}, {2, 1, 1}}));
+        System.out.println(new Solution2().longestIncreasingPath(new int[][]{{9, 9, 4}, {6, 6, 8}, {2, 1, 1}}));
+    }
 
+    /*
+    方法一：记忆化深度优先搜索
+     */
+    static class Solution {
+        public int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        public int rows, columns;
+
+        public int longestIncreasingPath(int[][] matrix) {
+            if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+                return 0;
+            }
+            rows = matrix.length;
+            columns = matrix[0].length;
+            int[][] memo = new int[rows][columns];
+            int ans = 0;
+            for (int i = 0; i < rows; ++i) {
+                for (int j = 0; j < columns; ++j) {
+                    ans = Math.max(ans, dfs(matrix, i, j, memo));
+                }
+            }
+            return ans;
+        }
+
+        public int dfs(int[][] matrix, int row, int column, int[][] memo) {
+            if (memo[row][column] != 0) {
+                return memo[row][column];
+            }
+            ++memo[row][column];
+            for (int[] dir : dirs) {
+                int newRow = row + dir[0], newColumn = column + dir[1];
+                if (newRow >= 0 && newRow < rows && newColumn >= 0 && newColumn < columns && matrix[newRow][newColumn] > matrix[row][column]) {
+                    memo[row][column] = Math.max(memo[row][column], dfs(matrix, newRow, newColumn, memo) + 1);
+                }
+            }
+            return memo[row][column];
+        }
+    }
+
+    /*
+    方法二：拓扑排序
+     */
+    static class Solution2 {
+        public int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        public int rows, columns;
+
+        public int longestIncreasingPath(int[][] matrix) {
+            if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+                return 0;
+            }
+            rows = matrix.length;
+            columns = matrix[0].length;
+            int[][] outdegrees = new int[rows][columns];
+            for (int i = 0; i < rows; ++i) {
+                for (int j = 0; j < columns; ++j) {
+                    for (int[] dir : dirs) {
+                        int newRow = i + dir[0], newColumn = j + dir[1];
+                        if (newRow >= 0 && newRow < rows && newColumn >= 0 && newColumn < columns && matrix[newRow][newColumn] > matrix[i][j]) {
+                            ++outdegrees[i][j];
+                        }
+                    }
+                }
+            }
+            Queue<int[]> queue = new LinkedList<int[]>();
+            for (int i = 0; i < rows; ++i) {
+                for (int j = 0; j < columns; ++j) {
+                    if (outdegrees[i][j] == 0) {
+                        queue.offer(new int[]{i, j});
+                    }
+                }
+            }
+            int ans = 0;
+            while (!queue.isEmpty()) {
+                ++ans;
+                int size = queue.size();
+                for (int i = 0; i < size; ++i) {
+                    int[] cell = queue.poll();
+                    int row = cell[0], column = cell[1];
+                    for (int[] dir : dirs) {
+                        int newRow = row + dir[0], newColumn = column + dir[1];
+                        if (newRow >= 0 && newRow < rows && newColumn >= 0 && newColumn < columns && matrix[newRow][newColumn] < matrix[row][column]) {
+                            --outdegrees[newRow][newColumn];
+                            if (outdegrees[newRow][newColumn] == 0) {
+                                queue.offer(new int[]{newRow, newColumn});
+                            }
+                        }
+                    }
+                }
+            }
+            return ans;
+        }
     }
 }
