1b844205613d13a6d6208f6dd07cbb170b711c8e
zhxh <zhxhcoder@gmail.com>
2022-09-02 15:21:45 +0800

    洗牌算法

--- app/src/main/java/com/zhxh/codeproj/leetcode/top145/LeetCode350.java
+++ app/src/main/java/com/zhxh/codeproj/leetcode/top145/LeetCode350.java
@@ -1,7 +1,98 @@
 package com.zhxh.codeproj.leetcode.top145;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+给你两个整数数组 nums1 和 nums2 ，请你以数组形式返回两数组的交集。返回结果中每个元素出现的次数，应与元素在两个数组中都出现的次数一致（如果出现次数不一致，则考虑取较小值）。可以不考虑输出结果的顺序。
+
+ 
+
+示例 1：
+
+输入：nums1 = [1,2,2,1], nums2 = [2,2]
+输出：[2,2]
+示例 2:
+
+输入：nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+输出：[4,9]
+ 
+
+提示：
+
+1 <= nums1.length, nums2.length <= 1000
+0 <= nums1[i], nums2[i] <= 1000
+ 
+
+进阶：
+
+如果给定的数组已经排好序呢？你将如何优化你的算法？
+如果 nums1 的大小比 nums2 小，哪种方法更优？
+如果 nums2 的元素存储在磁盘上，内存是有限的，并且你不能一次加载所有的元素到内存中，你该怎么办？
+
+ */
 public class LeetCode350 {
     public static void main(String[] args) {
+        System.out.println(Arrays.toString(new Solution().intersect(new int[]{1, 4, 9, 5}, new int[]{9, 4, 9, 8, 4})));
+        System.out.println(Arrays.toString(new Solution2().intersect(new int[]{1, 4, 9, 5}, new int[]{9, 4, 9, 8, 4})));
+    }
 
+    /*
+    方法一：哈希表
+     */
+    static class Solution {
+        public int[] intersect(int[] nums1, int[] nums2) {
+            if (nums1.length > nums2.length) {
+                return intersect(nums2, nums1);
+            }
+            Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+            for (int num : nums1) {
+                int count = map.getOrDefault(num, 0) + 1;
+                map.put(num, count);
+            }
+            int[] intersection = new int[nums1.length];
+            int index = 0;
+            for (int num : nums2) {
+                int count = map.getOrDefault(num, 0);
+                if (count > 0) {
+                    intersection[index++] = num;
+                    count--;
+                    if (count > 0) {
+                        map.put(num, count);
+                    } else {
+                        map.remove(num);
+                    }
+                }
+            }
+            return Arrays.copyOfRange(intersection, 0, index);
+        }
+    }
+
+    /*
+    方法二：排序 + 双指针
+    如果两个数组是有序的，则可以使用双指针的方法 得到两个数组的交集
+     */
+    static class Solution2 {
+        public int[] intersect(int[] nums1, int[] nums2) {
+            Arrays.sort(nums1);
+            Arrays.sort(nums2);
+            int length1 = nums1.length, length2 = nums2.length;
+            int[] intersection = new int[Math.min(length1, length2)];
+            int index1 = 0, index2 = 0, index = 0;
+            while (index1 < length1 && index2 < length2) {
+                if (nums1[index1] < nums2[index2]) {
+                    index1++;
+                } else if (nums1[index1] > nums2[index2]) {
+                    index2++;
+                } else {
+                    intersection[index] = nums1[index1];
+                    index1++;
+                    index2++;
+                    index++;
+                }
+            }
+            return Arrays.copyOfRange(intersection, 0, index);
+        }
     }
 }
--- app/src/main/java/com/zhxh/codeproj/leetcode/top145/LeetCode378.java
+++ app/src/main/java/com/zhxh/codeproj/leetcode/top145/LeetCode378.java
@@ -1,7 +1,128 @@
 package com.zhxh.codeproj.leetcode.top145;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/*
+给你一个 n x n 矩阵 matrix ，其中每行和每列元素均按升序排序，找到矩阵中第 k 小的元素。
+请注意，它是 排序后 的第 k 小元素，而不是第 k 个 不同 的元素。
+
+你必须找到一个内存复杂度优于 O(n2) 的解决方案。
+
+ 
+
+示例 1：
+
+输入：matrix = [[1,5,9],[10,11,13],[12,13,15]], k = 8
+输出：13
+解释：矩阵中的元素为 [1,5,9,10,11,12,13,13,15]，第 8 小元素是 13
+示例 2：
+
+输入：matrix = [[-5]], k = 1
+输出：-5
+ 
+
+提示：
+
+n == matrix.length
+n == matrix[i].length
+1 <= n <= 300
+-109 <= matrix[i][j] <= 109
+题目数据 保证 matrix 中的所有行和列都按 非递减顺序 排列
+1 <= k <= n2
+ 
+
+进阶：
+
+你能否用一个恒定的内存(即 O(1) 内存复杂度)来解决这个问题?
+你能在 O(n) 的时间复杂度下解决这个问题吗?这个方法对于面试来说可能太超前了，但是你会发现阅读这篇文章（ this paper ）很有趣。
+
+ */
 public class LeetCode378 {
     public static void main(String[] args) {
+        System.out.println(new Solution().kthSmallest(new int[][]{{1, 5, 9}, {10, 11, 13}, {12, 13, 15}}, 8));
+        System.out.println(new Solution2().kthSmallest(new int[][]{{1, 5, 9}, {10, 11, 13}, {12, 13, 15}}, 8));
+    }
 
+    /*
+    方法一：直接排序
+     */
+    static class Solution {
+        public int kthSmallest(int[][] matrix, int k) {
+            int rows = matrix.length, columns = matrix[0].length;
+            int[] sorted = new int[rows * columns];
+            int index = 0;
+            for (int[] row : matrix) {
+                for (int num : row) {
+                    sorted[index++] = num;
+                }
+            }
+            Arrays.sort(sorted);
+            return sorted[k - 1];
+        }
+    }
+
+
+    /*
+    方法二：归并排序
+    由题目给出的性质可知，这个矩阵的每一行均为一个有序数组。
+    问题即转化为从这 n 个有序数组中找第 k 大的数，可以想到利用归并排序的做法，归并到第 k 个数即可停止。
+
+     */
+    static class Solution2 {
+        public int kthSmallest(int[][] matrix, int k) {
+            PriorityQueue<int[]> pq = new PriorityQueue<int[]>(new Comparator<int[]>() {
+                public int compare(int[] a, int[] b) {
+                    return a[0] - b[0];
+                }
+            });
+            int n = matrix.length;
+            for (int i = 0; i < n; i++) {
+                pq.offer(new int[]{matrix[i][0], i, 0});
+            }
+            for (int i = 0; i < k - 1; i++) {
+                int[] now = pq.poll();
+                if (now[2] != n - 1) {
+                    pq.offer(new int[]{matrix[now[1]][now[2] + 1], now[1], now[2] + 1});
+                }
+            }
+            return pq.poll()[0];
+        }
+    }
+
+    /*
+    方法三：二分查找
+     */
+    static class Solution3 {
+        public int kthSmallest(int[][] matrix, int k) {
+            int n = matrix.length;
+            int left = matrix[0][0];
+            int right = matrix[n - 1][n - 1];
+            while (left < right) {
+                int mid = left + ((right - left) >> 1);
+                if (check(matrix, mid, k, n)) {
+                    right = mid;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            return left;
+        }
+
+        public boolean check(int[][] matrix, int mid, int k, int n) {
+            int i = n - 1;
+            int j = 0;
+            int num = 0;
+            while (i >= 0 && j < n) {
+                if (matrix[i][j] <= mid) {
+                    num += i + 1;
+                    j++;
+                } else {
+                    i--;
+                }
+            }
+            return num >= k;
+        }
     }
 }
