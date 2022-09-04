bb5600b10770dee1b0bfa3a9a3c50599d62ca2cf
zhxh <zhxhcoder@gmail.com>
2022-09-04 17:55:21 +0800

    寻找两个正序数组的中位数

--- app/src/main/java/com/zhxh/codeproj/leetcode/hot100/LeetCode04.java
+++ app/src/main/java/com/zhxh/codeproj/leetcode/hot100/LeetCode04.java
@@ -2,6 +2,8 @@ package com.zhxh.codeproj.leetcode.hot100;
 
 
 /*
+4. 寻找两个正序数组的中位数
+
 给定两个大小分别为 m 和 n 的正序（从小到大）数组nums1 和nums2。请你找出并返回这两个正序数组的 中位数 。
 算法的时间复杂度应该为 O(log (m+n)) 。
 示例 1：
@@ -22,11 +24,10 @@ nums2.length == n
  */
 public class LeetCode04 {
     public static void main(String[] args) {
-        int[] A = {1, 2};
-        int[] B = {3, 4};
-
-        System.out.println(new Solution().findMedianSortedArrays(A, B));
-        System.out.println(new Solution().findMedianSortedArrays1(A, B));
+        System.out.println(new Solution().findMedianSortedArrays(new int[]{1, 2}, new int[]{3, 4}));
+        System.out.println(new Solution2().findMedianSortedArrays(new int[]{1, 2}, new int[]{3, 4}));
+        System.out.println(new Solution3().findMedianSortedArrays(new int[]{1, 2}, new int[]{3, 4}));
+        System.out.println(new Solution4().findMedianSortedArrays(new int[]{1, 2}, new int[]{3, 4}));
     }
 
     static class Solution {
@@ -77,13 +78,15 @@ public class LeetCode04 {
             }
             return 0.0;
         }
+    }
 
-        public double findMedianSortedArrays1(int[] A, int[] B) {
+    static class Solution2 {
+        public double findMedianSortedArrays(int[] A, int[] B) {
             int leftLength = A.length;
             int rightLength = B.length;
             // 为了保证第一个数组比第二个数组小(或者相等)
             if (leftLength > rightLength) {
-                return findMedianSortedArrays1(B, A);
+                return findMedianSortedArrays(B, A);
             }
             // 分割线左边的所有元素需要满足的个数 m + (n - m + 1) / 2;
             // 两个数组长度之和为偶数时，当在长度之和上+1时，由于整除是向下取整，所以不会改变结果
@@ -155,6 +158,108 @@ public class LeetCode04 {
                 // 此处不能被向下取整，所以要强制转换为double类型
                 return (double) ((Math.max(nums1LeftMax, nums2LeftMax) + Math.min(nums1RightMin, nums2RightMin))) / 2;
             }
+        }
+    }
+
+    /*
+    方法一：二分查找
+     */
+    static class Solution3 {
+        public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+            int length1 = nums1.length, length2 = nums2.length;
+            int totalLength = length1 + length2;
+            if (totalLength % 2 == 1) {
+                int midIndex = totalLength / 2;
+                double median = getKthElement(nums1, nums2, midIndex + 1);
+                return median;
+            } else {
+                int midIndex1 = totalLength / 2 - 1, midIndex2 = totalLength / 2;
+                double median = (getKthElement(nums1, nums2, midIndex1 + 1) + getKthElement(nums1, nums2, midIndex2 + 1)) / 2.0;
+                return median;
+            }
+        }
+
+        public int getKthElement(int[] nums1, int[] nums2, int k) {
+            /* 主要思路：要找到第 k (k>1) 小的元素，那么就取 pivot1 = nums1[k/2-1] 和 pivot2 = nums2[k/2-1] 进行比较
+             * 这里的 "/" 表示整除
+             * nums1 中小于等于 pivot1 的元素有 nums1[0 .. k/2-2] 共计 k/2-1 个
+             * nums2 中小于等于 pivot2 的元素有 nums2[0 .. k/2-2] 共计 k/2-1 个
+             * 取 pivot = min(pivot1, pivot2)，两个数组中小于等于 pivot 的元素共计不会超过 (k/2-1) + (k/2-1) <= k-2 个
+             * 这样 pivot 本身最大也只能是第 k-1 小的元素
+             * 如果 pivot = pivot1，那么 nums1[0 .. k/2-1] 都不可能是第 k 小的元素。把这些元素全部 "删除"，剩下的作为新的 nums1 数组
+             * 如果 pivot = pivot2，那么 nums2[0 .. k/2-1] 都不可能是第 k 小的元素。把这些元素全部 "删除"，剩下的作为新的 nums2 数组
+             * 由于我们 "删除" 了一些元素（这些元素都比第 k 小的元素要小），因此需要修改 k 的值，减去删除的数的个数
+             */
+
+            int length1 = nums1.length, length2 = nums2.length;
+            int index1 = 0, index2 = 0;
+            int kthElement = 0;
+
+            while (true) {
+                // 边界情况
+                if (index1 == length1) {
+                    return nums2[index2 + k - 1];
+                }
+                if (index2 == length2) {
+                    return nums1[index1 + k - 1];
+                }
+                if (k == 1) {
+                    return Math.min(nums1[index1], nums2[index2]);
+                }
+
+                // 正常情况
+                int half = k / 2;
+                int newIndex1 = Math.min(index1 + half, length1) - 1;
+                int newIndex2 = Math.min(index2 + half, length2) - 1;
+                int pivot1 = nums1[newIndex1], pivot2 = nums2[newIndex2];
+                if (pivot1 <= pivot2) {
+                    k -= (newIndex1 - index1 + 1);
+                    index1 = newIndex1 + 1;
+                } else {
+                    k -= (newIndex2 - index2 + 1);
+                    index2 = newIndex2 + 1;
+                }
+            }
+        }
+    }
+
+    /*
+    方法二：划分数组
+     */
+    static class Solution4 {
+        public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+            if (nums1.length > nums2.length) {
+                return findMedianSortedArrays(nums2, nums1);
+            }
+
+            int m = nums1.length;
+            int n = nums2.length;
+            int left = 0, right = m;
+            // median1：前一部分的最大值
+            // median2：后一部分的最小值
+            int median1 = 0, median2 = 0;
+
+            while (left <= right) {
+                // 前一部分包含 nums1[0 .. i-1] 和 nums2[0 .. j-1]
+                // 后一部分包含 nums1[i .. m-1] 和 nums2[j .. n-1]
+                int i = (left + right) / 2;
+                int j = (m + n + 1) / 2 - i;
+
+                // nums_im1, nums_i, nums_jm1, nums_j 分别表示 nums1[i-1], nums1[i], nums2[j-1], nums2[j]
+                int nums_im1 = (i == 0 ? Integer.MIN_VALUE : nums1[i - 1]);
+                int nums_i = (i == m ? Integer.MAX_VALUE : nums1[i]);
+                int nums_jm1 = (j == 0 ? Integer.MIN_VALUE : nums2[j - 1]);
+                int nums_j = (j == n ? Integer.MAX_VALUE : nums2[j]);
+
+                if (nums_im1 <= nums_j) {
+                    median1 = Math.max(nums_im1, nums_jm1);
+                    median2 = Math.min(nums_i, nums_j);
+                    left = i + 1;
+                } else {
+                    right = i - 1;
+                }
+            }
+            return (m + n) % 2 == 0 ? (median1 + median2) / 2.0 : median1;
         }
     }
 }
