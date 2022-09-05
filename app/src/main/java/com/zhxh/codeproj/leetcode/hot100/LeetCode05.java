13d282cc80182a99b58bf50641cf4d7ce2c4b708
zhxh <zhxhcoder@gmail.com>
2022-09-05 10:44:26 +0800

    最长回文子串

--- app/src/main/java/com/zhxh/codeproj/leetcode/hot100/LeetCode05.java
+++ app/src/main/java/com/zhxh/codeproj/leetcode/hot100/LeetCode05.java
@@ -16,14 +16,14 @@ s 仅由数字和英文字母组成
 public class LeetCode05 {
     public static void main(String[] args) {
         System.out.println(new Solution().longestPalindrome("babbad"));
-        System.out.println(new Solution().longestPalindrome2("babbad"));
-        System.out.println(new Solution().longestPalindrome3("babbad"));
+        System.out.println(new Solution2().longestPalindrome("babbad"));
+        System.out.println(new Solution3().longestPalindrome("babbad"));
     }
 
+    /*
+    暴力解法
+     */
     static class Solution {
-        /*
-        暴力解法
-         */
         public String longestPalindrome(String s) {
             int len = s.length();
             if (len < 2) {
@@ -43,10 +43,7 @@ public class LeetCode05 {
                     }
                 }
             }
-
             return s.substring(begin, begin + maxLen);
-
-
         }
 
         private boolean validPalindromic(char[] charArray, int left, int right) {
@@ -59,14 +56,17 @@ public class LeetCode05 {
             }
             return true;
         }
+    }
 
-        /*
-        中心扩散法
-         - 枚举所有可能的回文子串的中心位置；
-         - 中心位置可能是一个字符，也有可能是两个相邻的字符
-         - 记录最长回文子串的相关变量
-         */
-        public String longestPalindrome2(String s) {
+
+    /*
+    中心扩散法
+     - 枚举所有可能的回文子串的中心位置；
+     - 中心位置可能是一个字符，也有可能是两个相邻的字符
+     - 记录最长回文子串的相关变量
+     */
+    static class Solution2 {
+        public String longestPalindrome(String s) {
             if (s == null || s.length() < 1) return "";
             int start = 0, end = 0;
             for (int i = 0; i < s.length(); i++) {
@@ -89,27 +89,29 @@ public class LeetCode05 {
             }
             return R - L - 1;
         }
+    }
 
 
-        /*
-        动态规划(回文的子串也是回文)
-        状态dp[i][j]表示子串s[i..j]是否为回文子串
-        得到状态转移方程：dp[i][j]=(s[i] == s[j]) and dp[i+1][j-1]
-        边界条件：j-1-(i+1)+1<2,整理得j-i<3 即 j-i+1<4  即s[i..j]长度为2或者3时，不用检查子串是否回文
+    /*
+    动态规划(回文的子串也是回文)
+    状态dp[i][j]表示子串s[i..j]是否为回文子串
+    得到状态转移方程：dp[i][j]=(s[i] == s[j]) and dp[i+1][j-1]
+    边界条件：j-1-(i+1)+1<2,整理得j-i<3 即 j-i+1<4  即s[i..j]长度为2或者3时，不用检查子串是否回文
 
-        动态规划，利用到了之前计算的值，所以相对快
+    动态规划，利用到了之前计算的值，所以相对快
 
-        初始化：dp[i][i] =true
-        输出：在得到一个状态的值为true的时候，记录起始位置和长度，填表完成以后再截取
+    初始化：dp[i][i] =true
+    输出：在得到一个状态的值为true的时候，记录起始位置和长度，填表完成以后再截取
 
-        状态规划就是填表，对角线肯定是true
-        状态转移方程：dp[i][j] = (s[i] == s[j]) and (j-i<3 or dp[i+1][j-1])
-        由于 dp[i][j]参考它左下方的值：
-        1，先升序填列
-        2，再升序填行
+    状态规划就是填表，对角线肯定是true
+    状态转移方程：dp[i][j] = (s[i] == s[j]) and (j-i<3 or dp[i+1][j-1])
+    由于 dp[i][j]参考它左下方的值：
+    1，先升序填列
+    2，再升序填行
 
-         */
-        public String longestPalindrome3(String s) {
+     */
+    static class Solution3 {
+        public String longestPalindrome(String s) {
             int len = s.length();
             if (len < 2) {
                 return s;
