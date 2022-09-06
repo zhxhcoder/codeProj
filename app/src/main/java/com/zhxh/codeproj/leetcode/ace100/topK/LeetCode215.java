61fe9e541b3b6610ae9d1a14cbd6adac921afde6
zhxh <zhxhcoder@gmail.com>
2022-09-06 15:13:05 +0800

    堆排序

--- app/src/main/java/com/zhxh/codeproj/leetcode/ace100/topK/LeetCode215.java
+++ app/src/main/java/com/zhxh/codeproj/leetcode/ace100/topK/LeetCode215.java
@@ -30,10 +30,9 @@ import java.util.Random;
 class LeetCode215 {
     public static void main(String[] args) {
         //todo topK问题
-        System.out.println(new Solution().findKthLargest(new int[]{3, 2, 1, 5, 6, 4}, 3));
-        System.out.println(new Solution2().findKthLargest(new int[]{3, 2, 1, 5, 6, 4}, 3));
-        System.out.println(new Solution3().findKthLargest(new int[]{3, 2, 1, 5, 6, 4}, 3));
-        System.out.println(new Solution4().findKthLargest(new int[]{3, 2, 1, 5, 6, 4}, 3));
+        System.out.println(new Solution().findKthLargest(new int[]{3, 2, 3, 1, 2, 4, 5, 5, 6}, 4));
+        System.out.println(new Solution2().findKthLargest(new int[]{3, 2, 3, 1, 2, 4, 5, 5, 6}, 4));
+        System.out.println(new Solution3().findKthLargest(new int[]{3, 2, 3, 1, 2, 4, 5, 5, 6}, 4));
     }
 
     /*
@@ -48,35 +47,43 @@ class LeetCode215 {
      */
     static class Solution {
         public int findKthLargest(int[] nums, int k) {
-            if (nums == null || nums.length < k) {
-                throw new IllegalArgumentException("找不到了");
-            }
             int heapSize = nums.length;
             buildMaxHeap(nums, heapSize);
+            //建堆完毕后，nums【0】为最大元素。逐个删除堆顶元素，直到删除了k-1个。
             for (int i = nums.length - 1; i >= nums.length - k + 1; --i) {
+                //先将堆的最后一个元素与堆顶元素交换，由于此时堆的性质被破坏，需对此时的根节点进行向下调整操作。
                 swap(nums, 0, i);
+                //相当于删除堆顶元素，此时长度变为nums.length-2。即下次循环的i
                 --heapSize;
                 maxHeapify(nums, 0, heapSize);
             }
             return nums[0];
         }
 
+        //
         public void buildMaxHeap(int[] a, int heapSize) {
-            for (int i = heapSize / 2; i >= 0; --i) {
+            //从最后一个父节点位置开始调整每一个节点的子树。
+            //数组长度为heapSize，因此最后一个节点的位置为heapSize-1，所以父节点的位置为（heapSize-1-1）/2。
+            for (int i = (heapSize - 2) / 2; i >= 0; --i) {
                 maxHeapify(a, i, heapSize);
             }
         }
 
-        public void maxHeapify(int[] a, int i, int heapSize) {
-            int l = i * 2 + 1, r = i * 2 + 2, largest = i;
-            if (l < heapSize && a[l] > a[largest]) {
-                largest = l;
+        public void maxHeapify(int[] a, int root, int heapSize) {      //调整当前结点和子节点的顺序。
+            //left和right表示当前父节点i的两个左右子节点。
+            int left = root * 2 + 1, right = root * 2 + 2, largest = root;
+            //如果左子点在数组内，且比当前父节点大，则将最大值的指针指向左子点。
+            if (left < heapSize && a[left] > a[largest]) {
+                largest = left;
             }
-            if (r < heapSize && a[r] > a[largest]) {
-                largest = r;
+            //如果右子点在数组内，且比当前父节点大，则将最大值的指针指向右子点。
+            if (right < heapSize && a[right] > a[largest]) {
+                largest = right;
             }
-            if (largest != i) {
-                swap(a, i, largest);
+            //如果最大值的指针不是父节点，则交换父节点和当前最大值指针指向的子节点。
+            if (largest != root) {
+                swap(a, root, largest);
+                //由于交换了父节点和子节点，因此可能对子节点的子树造成影响，所以对子节点的子树进行调整。
                 maxHeapify(a, largest, heapSize);
             }
         }
@@ -155,51 +162,4 @@ class LeetCode215 {
         }
     }
 
-    static class Solution4 {
-        public int findKthLargest(int[] nums, int k) {
-            int heapSize = nums.length;
-            buildMaxHeap(nums, heapSize);
-            //建堆完毕后，nums【0】为最大元素。逐个删除堆顶元素，直到删除了k-1个。
-            for (int i = nums.length - 1; i >= nums.length - k + 1; --i) {
-                //先将堆的最后一个元素与堆顶元素交换，由于此时堆的性质被破坏，需对此时的根节点进行向下调整操作。
-                swap(nums, 0, i);
-                //相当于删除堆顶元素，此时长度变为nums.length-2。即下次循环的i
-                --heapSize;
-                maxHeapify(nums, 0, heapSize);
-            }
-            return nums[0];
-        }
-
-        public void buildMaxHeap(int[] a, int heapSize) {
-            //从最后一个父节点位置开始调整每一个节点的子树。数组长度为heasize，因此最后一个节点的位置为heapsize-1，所以父节点的位置为heapsize-1-1/2。
-            for (int i = (heapSize - 2) / 2; i >= 0; --i) {
-                maxHeapify(a, i, heapSize);
-            }
-        }
-
-        public void maxHeapify(int[] a, int i, int heapSize) {      //调整当前结点和子节点的顺序。
-            //left和right表示当前父节点i的两个左右子节点。
-            int left = i * 2 + 1, right = i * 2 + 2, largest = i;
-            //如果左子点在数组内，且比当前父节点大，则将最大值的指针指向左子点。
-            if (left < heapSize && a[left] > a[largest]) {
-                largest = left;
-            }
-            //如果右子点在数组内，且比当前父节点大，则将最大值的指针指向右子点。
-            if (right < heapSize && a[right] > a[largest]) {
-                largest = right;
-            }
-            //如果最大值的指针不是父节点，则交换父节点和当前最大值指针指向的子节点。
-            if (largest != i) {
-                swap(a, i, largest);
-                //由于交换了父节点和子节点，因此可能对子节点的子树造成影响，所以对子节点的子树进行调整。
-                maxHeapify(a, largest, heapSize);
-            }
-        }
-
-        public void swap(int[] a, int i, int j) {
-            int temp = a[i];
-            a[i] = a[j];
-            a[j] = temp;
-        }
-    }
 }
