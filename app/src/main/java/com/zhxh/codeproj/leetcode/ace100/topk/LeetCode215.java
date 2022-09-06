2985925f049ea22d231801dafca0cc9eec326959
zhxh <zhxhcoder@gmail.com>
2022-09-06 13:25:39 +0800

    前 K 个高频元素

--- app/src/main/java/com/zhxh/codeproj/leetcode/ace100/topk/LeetCode215.java
+++ app/src/main/java/com/zhxh/codeproj/leetcode/ace100/topk/LeetCode215.java
@@ -4,13 +4,12 @@ import java.util.PriorityQueue;
 import java.util.Random;
 
 /*
+215. 数组中的第K个最大元素
 给定整数数组 nums 和整数 k，请返回数组中第 k 个最大的元素。
 
 请注意，你需要找的是数组排序后的第 k 个最大的元素，而不是第 k 个不同的元素。
 
 你必须设计并实现时间复杂度为 O(n) 的算法解决此问题。
-
-
 
 示例 1:
 
@@ -38,9 +37,19 @@ class LeetCode215 {
 
     /*
     基于堆排序的选择方法
+    堆是一种完全二叉树
+    完全二叉树：若设二叉树的深度为h，除第 h 层外，其它各层 (1～h-1) 的结点数都达到最大个数，第 h 层所有的结点都连续集中在最左边，这就是完全二叉树。
+    之前说到，堆是一种完全二叉树，但是否就意味着我们真的要用树来表示它呢？
+    答案是否定的，因为完全二叉树有其非常卓越的性质：对于任意一个父节点的序号n来说（这里n从0算），它的子节点的序号一定是2n+1,2n+2，因此我们可以直接用数组来表示一个堆。
+    所以对于上图来说，我们可以用[0,1,3,2,4,6,7,8,9,5,10]来表示这个堆。
+    最小堆要求，对于任意一个父结点来说，其子结点的值都大于这个父节点,
+    同理，最大堆就是说，其子节点的值都小于这个父节点。
      */
     static class Solution {
         public int findKthLargest(int[] nums, int k) {
+            if (nums == null || nums.length < k) {
+                throw new IllegalArgumentException("找不到了");
+            }
             int heapSize = nums.length;
             buildMaxHeap(nums, heapSize);
             for (int i = nums.length - 1; i >= nums.length - k + 1; --i) {
@@ -78,8 +87,14 @@ class LeetCode215 {
         }
     }
 
+    /*
+    优先队列
+     */
     static class Solution2 {
         public int findKthLargest(int[] nums, int k) {
+            if (nums == null || nums.length < k) {
+                throw new IllegalArgumentException("找不到了");
+            }
             // 初始化最小元素优先堆
             PriorityQueue<Integer> heap = new PriorityQueue<Integer>((n1, n2) -> n1 - n2);
             //在堆中保留 k 个最大元素
@@ -96,9 +111,13 @@ class LeetCode215 {
     基于快速排序的选择方法
      */
     static class Solution3 {
+
         Random random = new Random();
 
         public int findKthLargest(int[] nums, int k) {
+            if (nums == null || nums.length < k) {
+                throw new IllegalArgumentException("找不到了");
+            }
             return quickSelect(nums, 0, nums.length - 1, nums.length - k);
         }
 
