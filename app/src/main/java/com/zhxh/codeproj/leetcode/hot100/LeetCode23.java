17ea8acbc269d70f8b23bb5c3f4adeac28309d6f
zhxh <zhxhcoder@gmail.com>
2022-09-07 11:42:09 +0800

    链表合并

--- app/src/main/java/com/zhxh/codeproj/leetcode/hot100/LeetCode23.java
+++ app/src/main/java/com/zhxh/codeproj/leetcode/hot100/LeetCode23.java
@@ -2,10 +2,14 @@ package com.zhxh.codeproj.leetcode.hot100;
 
 import com.zhxh.codeproj.leetcode.__base.ListNode;
 
+import java.util.PriorityQueue;
+
 /*
+23. 合并K个升序链表
 给你一个链表数组，每个链表都已经按升序排列。
 
 请你将所有链表合并到一个升序链表中，返回合并后的链表。
+
 
 示例 1：
 
@@ -45,6 +49,24 @@ public class LeetCode23 {
                 ListNode.buildNode(new int[]{1, 3, 4}),
                 ListNode.buildNode(new int[]{2, 6})}
         ));
+
+        ListNode.printNode(new Solution2().mergeKLists(new ListNode[]{
+                ListNode.buildNode(new int[]{1, 4, 5}),
+                ListNode.buildNode(new int[]{1, 3, 4}),
+                ListNode.buildNode(new int[]{2, 6})}
+        ));
+
+        ListNode.printNode(new Solution3().mergeKLists(new ListNode[]{
+                ListNode.buildNode(new int[]{1, 4, 5}),
+                ListNode.buildNode(new int[]{1, 3, 4}),
+                ListNode.buildNode(new int[]{2, 6})}
+        ));
+
+        ListNode.printNode(new Solution4().mergeKLists(new ListNode[]{
+                ListNode.buildNode(new int[]{1, 4, 5}),
+                ListNode.buildNode(new int[]{1, 3, 4}),
+                ListNode.buildNode(new int[]{2, 6})}
+        ));
     }
 
     static class Solution {
@@ -71,6 +93,121 @@ public class LeetCode23 {
                 l2.next = mergeTwoLists(l1, l2.next);
                 return l2;
             }
+        }
+    }
+
+    /*
+    方法一：顺序合并
+    我们可以想到一种最朴素的方法：用一个变量 ans 来维护以及合并的链表，
+    第 i 次循环把第 i 个链表和 ans 合并，答案保存到 ans 中。
+     */
+    static class Solution2 {
+        public ListNode mergeKLists(ListNode[] lists) {
+            ListNode ans = null;
+            for (int i = 0; i < lists.length; ++i) {
+                ans = mergeTwoLists(ans, lists[i]);
+            }
+            return ans;
+        }
+
+        public ListNode mergeTwoLists(ListNode a, ListNode b) {
+            if (a == null || b == null) {
+                return a != null ? a : b;
+            }
+            ListNode head = new ListNode(0);
+            ListNode tail = head, aPtr = a, bPtr = b;
+            while (aPtr != null && bPtr != null) {
+                if (aPtr.val < bPtr.val) {
+                    tail.next = aPtr;
+                    aPtr = aPtr.next;
+                } else {
+                    tail.next = bPtr;
+                    bPtr = bPtr.next;
+                }
+                tail = tail.next;
+            }
+            tail.next = (aPtr != null ? aPtr : bPtr);
+            return head.next;
+        }
+    }
+
+    /*
+    方法二：分治合并
+     */
+    static class Solution3 {
+        public ListNode mergeKLists(ListNode[] lists) {
+            return merge(lists, 0, lists.length - 1);
+        }
+
+        public ListNode merge(ListNode[] lists, int l, int r) {
+            if (l == r) {
+                return lists[l];
+            }
+            if (l > r) {
+                return null;
+            }
+            int mid = (l + r) >> 1;
+            return mergeTwoLists(merge(lists, l, mid), merge(lists, mid + 1, r));
+        }
+
+        public ListNode mergeTwoLists(ListNode a, ListNode b) {
+            if (a == null || b == null) {
+                return a != null ? a : b;
+            }
+            ListNode head = new ListNode(0);
+            ListNode tail = head, aPtr = a, bPtr = b;
+            while (aPtr != null && bPtr != null) {
+                if (aPtr.val < bPtr.val) {
+                    tail.next = aPtr;
+                    aPtr = aPtr.next;
+                } else {
+                    tail.next = bPtr;
+                    bPtr = bPtr.next;
+                }
+                tail = tail.next;
+            }
+            tail.next = (aPtr != null ? aPtr : bPtr);
+            return head.next;
+        }
+    }
+
+    /*
+    方法三：使用优先队列合并
+     */
+    static class Solution4 {
+        static class Status implements Comparable<Status> {
+            int val;
+            ListNode ptr;
+
+            Status(int val, ListNode ptr) {
+                this.val = val;
+                this.ptr = ptr;
+            }
+
+            public int compareTo(Status status2) {
+                return this.val - status2.val;
+            }
+        }
+
+        PriorityQueue<Status> queue = new PriorityQueue<Status>();
+
+        public ListNode mergeKLists(ListNode[] lists) {
+            for (ListNode node : lists) {
+                if (node != null) {
+                    queue.offer(new Status(node.val, node));
+                }
+            }
+            ListNode head = new ListNode(0);
+            ListNode tail = head;
+            while (!queue.isEmpty()) {
+                Status f = queue.poll();
+                tail.next = f.ptr;
+                tail = tail.next;
+                if (f.ptr.next != null) {
+                    queue.offer(new Status(f.ptr.next.val, f.ptr.next));
+                }
+            }
+            return head.next;
         }
     }
 }
