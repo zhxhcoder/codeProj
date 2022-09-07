f868cfde598726633f557d3ed2666e9b1160591c
zhxh <zhxhcoder@gmail.com>
2022-09-07 15:31:58 +0800

    搜索旋转排序数组

--- app/src/main/java/com/zhxh/codeproj/leetcode/hot100/LeetCode33.java
+++ app/src/main/java/com/zhxh/codeproj/leetcode/hot100/LeetCode33.java
@@ -1,19 +1,22 @@
 package com.zhxh.codeproj.leetcode.hot100;
 
 /*
+33. 搜索旋转排序数组
 整数数组 nums 按升序排列，数组中的值 互不相同 。
 
-在传递给函数之前，nums 在预先未知的某个下标 k（0 <= k < nums.length）上进行了 旋转，使数组变为 [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]]（下标 从 0 开始 计数）。例如， [0,1,2,4,5,6,7] 在下标 3 处经旋转后可能变为[4,5,6,7,0,1,2] 。
+在传递给函数之前，nums 在预先未知的某个下标 k（0 <= k < nums.length）上进行了 旋转，使数组变为 [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]]（下标 从 0 开始 计数）。例如， [0,1,2,4,5,6,7] 在下标 3 处经旋转后可能变为 [4,5,6,7,0,1,2] 。
 
-给你 旋转后 的数组 nums 和一个整数 target ，如果 nums 中存在这个目标值 target ，则返回它的下标，否则返回-1。
+给你 旋转后 的数组 nums 和一个整数 target ，如果 nums 中存在这个目标值 target ，则返回它的下标，否则返回 -1 。
 
 你必须设计一个时间复杂度为 O(log n) 的算法解决此问题。
+
+
 
 示例 1：
 
 输入：nums = [4,5,6,7,0,1,2], target = 0
 输出：4
-示例2：
+示例 2：
 
 输入：nums = [4,5,6,7,0,1,2], target = 3
 输出：-1
@@ -34,10 +37,57 @@ nums 中的每个值都 独一无二
 public class LeetCode33 {
     public static void main(String[] args) {
         System.out.println(new Solution().search(new int[]{4, 5, 6, 7, 0, 1, 2}, 1));
-        System.out.println(new Solution().search2(new int[]{4, 5, 6, 7, 0, 1, 2}, 1));
+        System.out.println(new Solution2().search(new int[]{4, 5, 6, 7, 0, 1, 2}, 1));
     }
 
+    /*
+    二分搜索
+    -关键词：排序，搜索
+    -模式识别：有序或部分有序，基本使用二分搜索及其变种
+    -算法描述："丢弃"一半的数据
+
+     可以发现的是，我们将数组从中间分开成左右两部分的时候，一定有一部分的数组是有序的。拿示例来看，我们从 6 这个位置分开以后数组变成了 [4, 5, 6] 和 [7, 0, 1, 2] 两个部分，其中左边 [4, 5, 6] 这个部分的数组是有序的，其他也是如此。
+
+     这启示我们可以在常规二分查找的时候查看当前 mid 为分割位置分割出来的两个部分 [l, mid] 和 [mid + 1, r] 哪个部分是有序的，并根据有序的那个部分确定我们该如何改变二分查找的上下界，因为我们能够根据有序的那部分判断出 target 在不在这个部分：
+
+     如果 [l, mid - 1] 是有序数组，且 target 的大小满足[nums[l],nums[mid])，则我们应该将搜索范围缩小至 [l, mid - 1]，否则在 [mid + 1, r] 中寻找。
+     如果 [mid, r] 是有序数组，且 target 的大小满足(nums[mid+1],nums[r]]，则我们应该将搜索范围缩小至 [mid + 1, r]，否则在 [l, mid - 1] 中寻找。
+     */
     static class Solution {
+        public int search(int[] nums, int target) {
+            int n = nums.length;
+            if (n == 0) {
+                return -1;
+            }
+            if (n == 1) {
+                return nums[0] == target ? 0 : -1;
+            }
+            int l = 0, r = n - 1;
+            while (l <= r) {
+                int mid = (l + r) / 2;
+                if (nums[mid] == target) {
+                    return mid;
+                }
+                if (nums[0] <= nums[mid]) {
+                    if (nums[0] <= target && target < nums[mid]) {
+                        r = mid - 1;
+                    } else {
+                        l = mid + 1;
+                    }
+                } else {
+                    if (nums[mid] < target && target <= nums[n - 1]) {
+                        l = mid + 1;
+                    } else {
+                        r = mid - 1;
+                    }
+                }
+            }
+            return -1;
+        }
+    }
+
+
+    static class Solution2 {
         //递归求解
         public int search(int[] nums, int target) {
             //对非法输入或不满足helper的情况提前处理
@@ -76,52 +126,6 @@ public class LeetCode33 {
             }
             //递归的求解
             return helper(nums, target, left, right);
-        }
-
-        /*
-        二分搜索
-        -关键词：排序，搜索
-        -模式识别：有序或部分有序，基本使用二分搜索及其变种
-        -算法描述："丢弃"一半的数据
-
-
-可以发现的是，我们将数组从中间分开成左右两部分的时候，一定有一部分的数组是有序的。拿示例来看，我们从 6 这个位置分开以后数组变成了 [4, 5, 6] 和 [7, 0, 1, 2] 两个部分，其中左边 [4, 5, 6] 这个部分的数组是有序的，其他也是如此。
-
-这启示我们可以在常规二分查找的时候查看当前 mid 为分割位置分割出来的两个部分 [l, mid] 和 [mid + 1, r] 哪个部分是有序的，并根据有序的那个部分确定我们该如何改变二分查找的上下界，因为我们能够根据有序的那部分判断出 target 在不在这个部分：
-
-如果 [l, mid - 1] 是有序数组，且 target 的大小满足[nums[l],nums[mid])，则我们应该将搜索范围缩小至 [l, mid - 1]，否则在 [mid + 1, r] 中寻找。
-如果 [mid, r] 是有序数组，且 target 的大小满足(nums[mid+1],nums[r]]，则我们应该将搜索范围缩小至 [mid + 1, r]，否则在 [l, mid - 1] 中寻找。
-
-         */
-        public int search2(int[] nums, int target) {
-            int n = nums.length;
-            if (n == 0) {
-                return -1;
-            }
-            if (n == 1) {
-                return nums[0] == target ? 0 : -1;
-            }
-            int l = 0, r = n - 1;
-            while (l <= r) {
-                int mid = (l + r) / 2;
-                if (nums[mid] == target) {
-                    return mid;
-                }
-                if (nums[0] <= nums[mid]) {
-                    if (nums[0] <= target && target < nums[mid]) {
-                        r = mid - 1;
-                    } else {
-                        l = mid + 1;
-                    }
-                } else {
-                    if (nums[mid] < target && target <= nums[n - 1]) {
-                        l = mid + 1;
-                    } else {
-                        r = mid - 1;
-                    }
-                }
-            }
-            return -1;
         }
     }
 }
