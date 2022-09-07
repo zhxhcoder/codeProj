3be43386e2efd78e05d6d99df9c19724d8ee893f
zhxh <zhxhcoder@gmail.com>
2022-09-07 15:27:56 +0800

    最长有效括号

--- app/src/main/java/com/zhxh/codeproj/leetcode/hot100/LeetCode32.java
+++ app/src/main/java/com/zhxh/codeproj/leetcode/hot100/LeetCode32.java
@@ -1,9 +1,14 @@
 package com.zhxh.codeproj.leetcode.hot100;
 
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.Stack;
 
 /*
-给你一个只包含 '('和 ')'的字符串，找出最长有效（格式正确且连续）括号子串的长度。
+32. 最长有效括号
+给你一个只包含 '(' 和 ')' 的字符串，找出最长有效（格式正确且连续）括号子串的长度。
+
+
 
 示例 1：
 
@@ -20,6 +25,7 @@ import java.util.Stack;
 输入：s = ""
 输出：0
 
+
 提示：
 
 0 <= s.length <= 3 * 104
@@ -29,9 +35,93 @@ s[i] 为 '(' 或 ')'
 public class LeetCode32 {
     public static void main(String[] args) {
         System.out.println(new Solution().longestValidParentheses(")()())"));
+        System.out.println(new Solution2().longestValidParentheses(")()())"));
+        System.out.println(new Solution3().longestValidParentheses(")()())"));
+        System.out.println(new Solution4().longestValidParentheses(")()())"));
     }
 
+    /*
+    方法一：动态规划
+    我们定义dp[i]表示以下标i字符结尾的最长有效括号的长度。
+     */
     static class Solution {
+        public int longestValidParentheses(String s) {
+            int maxans = 0;
+            int[] dp = new int[s.length()];
+            for (int i = 1; i < s.length(); i++) {
+                if (s.charAt(i) == ')') {
+                    if (s.charAt(i - 1) == '(') {
+                        dp[i] = (i >= 2 ? dp[i - 2] : 0) + 2;
+                    } else if (i - dp[i - 1] > 0 && s.charAt(i - dp[i - 1] - 1) == '(') {
+                        dp[i] = dp[i - 1] + ((i - dp[i - 1]) >= 2 ? dp[i - dp[i - 1] - 2] : 0) + 2;
+                    }
+                    maxans = Math.max(maxans, dp[i]);
+                }
+            }
+            return maxans;
+        }
+    }
+
+    /*
+    方法二：栈
+     */
+    static class Solution2 {
+        public int longestValidParentheses(String s) {
+            int maxans = 0;
+            Deque<Integer> stack = new LinkedList<Integer>();
+            stack.push(-1);
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) == '(') {
+                    stack.push(i);
+                } else {
+                    stack.pop();
+                    if (stack.isEmpty()) {
+                        stack.push(i);
+                    } else {
+                        maxans = Math.max(maxans, i - stack.peek());
+                    }
+                }
+            }
+            return maxans;
+        }
+    }
+
+    /*
+    方法三：不需要额外的空间
+     */
+    static class Solution3 {
+        public int longestValidParentheses(String s) {
+            int left = 0, right = 0, maxlength = 0;
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) == '(') {
+                    left++;
+                } else {
+                    right++;
+                }
+                if (left == right) {
+                    maxlength = Math.max(maxlength, 2 * right);
+                } else if (right > left) {
+                    left = right = 0;
+                }
+            }
+            left = right = 0;
+            for (int i = s.length() - 1; i >= 0; i--) {
+                if (s.charAt(i) == '(') {
+                    left++;
+                } else {
+                    right++;
+                }
+                if (left == right) {
+                    maxlength = Math.max(maxlength, 2 * left);
+                } else if (left > right) {
+                    left = right = 0;
+                }
+            }
+            return maxlength;
+        }
+    }
+
+    static class Solution4 {
         public int longestValidParentheses(String s) {
             int maxans = 0;
             Stack<Integer> stack = new Stack<>();
