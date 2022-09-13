d7abf5ecca68a1957aa7aca4d55df9ec150db3e1
zhxh <zhxhcoder@gmail.com>
2022-09-13 13:25:32 +0800

    饿汉式单例

--- app/src/main/java/com/zhxh/codeproj/designpattern/singleton/enums/SingletonEnum.java
+++ app/src/main/java/com/zhxh/codeproj/designpattern/singleton/enums/SingletonEnum.java
@@ -3,14 +3,13 @@ package com.zhxh.codeproj.designpattern.singleton.enums;
 /**
  * Created by zhxh on 2016/10/28.
  * 8. 枚举[极推荐使用]
- *
+ * <p>
  * 这里SingletonEnum.instance
  * 这里的instance即为SingletonEnum类型的引用所以得到它就可以调用枚举中的方法了。
- 借助JDK1.5中添加的枚举来实现单例模式。不仅能避免多线程同步问题，而且还能防止反序列化重新创建新的对象
+ * 借助JDK1.5中添加的枚举来实现单例模式。不仅能避免多线程同步问题，而且还能防止反序列化重新创建新的对象
  */
 
 public enum SingletonEnum {
-
     instance;
 
     private SingletonEnum() {
@@ -19,7 +18,5 @@ public enum SingletonEnum {
     public void whateverMethod() {
 
     }
-
     // SingletonEnum.instance.method();
-
 }
