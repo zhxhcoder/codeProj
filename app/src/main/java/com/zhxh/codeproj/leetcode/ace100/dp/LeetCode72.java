e0f2d2113411536392a04d1dd1acc995f432c260
zhxh <zhxhcoder@gmail.com>
2022-09-15 16:40:42 +0800

    动态规划专题

--- app/src/main/java/com/zhxh/codeproj/leetcode/ace100/dp/LeetCode72.java
+++ app/src/main/java/com/zhxh/codeproj/leetcode/ace100/dp/LeetCode72.java
@@ -148,29 +148,29 @@ public class LeetCode72 {
             }
 
             // DP 数组
-            int[][] D = new int[n + 1][m + 1];
+            int[][] dp = new int[n + 1][m + 1];
 
             // 边界状态初始化
             for (int i = 0; i < n + 1; i++) {
-                D[i][0] = i;
+                dp[i][0] = i;
             }
             for (int j = 0; j < m + 1; j++) {
-                D[0][j] = j;
+                dp[0][j] = j;
             }
 
             // 计算所有 DP 值
             for (int i = 1; i < n + 1; i++) {
                 for (int j = 1; j < m + 1; j++) {
-                    int left = D[i - 1][j] + 1;
-                    int down = D[i][j - 1] + 1;
-                    int left_down = D[i - 1][j - 1];
+                    int left = dp[i - 1][j] + 1;
+                    int down = dp[i][j - 1] + 1;
+                    int left_down = dp[i - 1][j - 1];
                     if (word1.charAt(i - 1) != word2.charAt(j - 1)) {
                         left_down += 1;
                     }
-                    D[i][j] = Math.min(left, Math.min(down, left_down));
+                    dp[i][j] = Math.min(left, Math.min(down, left_down));
                 }
             }
-            return D[n][m];
+            return dp[n][m];
         }
     }
 
