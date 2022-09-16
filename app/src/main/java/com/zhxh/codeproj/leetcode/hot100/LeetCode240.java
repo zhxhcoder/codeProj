7c052b43205336fed07063dec2f39ac1b5bbc1eb
zhxh <zhxhcoder@gmail.com>
2022-09-16 17:12:14 +0800

    二维数组查找

--- app/src/main/java/com/zhxh/codeproj/leetcode/hot100/LeetCode240.java
+++ app/src/main/java/com/zhxh/codeproj/leetcode/hot100/LeetCode240.java
@@ -1,7 +1,8 @@
 package com.zhxh.codeproj.leetcode.hot100;
 
 /*
-编写一个高效的算法来搜索mxn矩阵 matrix 中的一个目标值 target 。该矩阵具有以下特性：
+240. 搜索二维矩阵 II
+编写一个高效的算法来搜索 m x n 矩阵 matrix 中的一个目标值 target 。该矩阵具有以下特性：
 
 每行的元素从左到右升序排列。
 每列的元素从上到下升序排列。
@@ -24,11 +25,10 @@ package com.zhxh.codeproj.leetcode.hot100;
 m == matrix.length
 n == matrix[i].length
 1 <= n, m <= 300
--109<= matrix[i][j] <= 109
+-109 <= matrix[i][j] <= 109
 每行的所有元素从左到右升序排列
 每列的所有元素从上到下升序排列
--109<= target <= 109
-
+-109 <= target <= 109
  */
 public class LeetCode240 {
     public static void main(String[] args) {
