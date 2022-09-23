4293bc127fda25632ae3ee4101c3c98ad37c3fcc
zhxh <zhxhcoder@gmail.com>
2022-09-23 10:38:52 +0800

    join方法

---
+++ app/src/main/java/com/zhxh/codeproj/multithread/join/JoinDemo.java
@@ -0,0 +1,12 @@
+package com.zhxh.codeproj.multithread.join;
+
+
+class JoinDemo implements Runnable {
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("线程1第" + i + "次执行！");
+        }
+    }
+}
---
+++ app/src/main/java/com/zhxh/codeproj/multithread/join/JoinTest.java
@@ -0,0 +1,23 @@
+package com.zhxh.codeproj.multithread.join;
+
+
+/*
+join() 　　join()方法会使当前线程等待调用join()方法的线程结束后才能继续执行，例如：
+ */
+public class JoinTest {
+    public static void main(String[] args) {
+        Thread thread = new Thread(new JoinDemo());
+        thread.start();
+
+        for (int i = 0; i < 20; i++) {
+            System.out.println("主线程第" + i + "次执行！");
+            if (i >= 2)
+                try {
+                    // t1线程合并到主线程中，主线程停止执行过程，转而执行t1线程，直到t1执行完毕后继续。
+                    thread.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+        }
+    }
+}
